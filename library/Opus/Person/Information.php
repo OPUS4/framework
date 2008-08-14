7590ed27de58fd23cf856a95f2e21f0fb44d6ebe
Ralf Claußnitzer <ralf.claussnitzer@slub-dresden.de>
2008-08-14 07:55:32 +0000

    Improved documentation.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@583 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- library/Opus/Person/Information.php
+++ library/Opus/Person/Information.php
@@ -15,7 +15,16 @@ class Opus_Person_Information {
     /**
      * Use given person information to create a new record. Information is given
      * in the form of an array containing the keys firstName, lastName, placeOfBirth,
-     * dateOfBirth, email and academicTitle.
+     * dateOfBirth, email and academicTitle, like so:
+     *
+     * array(
+     *     firstName,    - a person's first name
+     *     lastName,     - a person's last name
+     *     placeOfBirth, - birthplace
+     *     dateOfBith,   - birthday
+     *     email,        - contact email address
+     *     academicTitle - (Optional) a person's academic title
+     * )
      *
      * @param array $person Array with person information.
      * @throws InvalidArgumentException Is thrown on invalid arguments.
@@ -28,6 +37,16 @@ class Opus_Person_Information {
     /**
      * Use given person information to update a record. The given array has to have
      * a 'Id' key set to a valid person identifier.
+     *
+     * array(
+     *     Id,           - (Required) person's identifier
+     *     firstName,    - (Optional) a person's first name
+     *     lastName,     - (Optional) a person's last name
+     *     placeOfBirth, - (Optional) birthplace
+     *     dateOfBith,   - (Optional) birthday
+     *     email,        - (Optional) contact email address
+     *     academicTitle - (Optional) a person's academic title
+     * )
      *
      * @param array $person Array with person information.
      * @throws InvalidArgumentException Is thrown on invalid arguments.
@@ -52,6 +71,22 @@ class Opus_Person_Information {
      * Returns an array of person records according to given filter criteria or
      * identifier.
      *
+     * The criteria array might look like this:
+     * array(
+     *     firstName,    - (Optional) a person's first name
+     *     lastName,     - (Optional) a person's last name
+     *     placeOfBirth, - (Optional) birthplace
+     *     dateOfBith,   - (Optional) birthday
+     *     email,        - (Optional) contact email address
+     *     academicTitle - (Optional) a person's academic title
+     * )
+     *
+     * The returned array is empty if no records have been found. Otherwise it
+     * will contain one or more array(s) of personal information simliar to the criteria
+     * array.
+     *
+     * TODO Add wildcard support.
+     *
      * @param array|integer $criteria Array with filter criteria or valid person identifier.
      * @throws InvalidArgumentException Is thrown on invalid arguments.
      * @return array Array of person records matching the criteria.
@@ -62,6 +97,18 @@ class Opus_Person_Information {
 
     /**
      * Return an array containing all person records available.
+     *
+     * The returned array is either empty or consists of arrays simliar to
+     * this one:
+     *
+     * array(
+     *     firstName,    - a person's first name
+     *     lastName,     - a person's last name
+     *     placeOfBirth, - birthplace
+     *     dateOfBith,   - birthday
+     *     email,        - contact email address
+     *     academicTitle - (Optional) a person's academic title
+     * )
      *
      * @return array Array of person information records.
      */
