a8bf186985f889a5a35dc5404fe89c955999d3c9
Tobias Leidinger <tobias.leidinger@gmail.com>
2008-08-21 13:02:00 +0000

    added files for opus_form component
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@608 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ library/Opus/Form/Field.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * Created on 21.08.2008
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+class Field {
+    protected $type;
+    protected $value;
+    protected $valueLanguage;
+    protected $valueAdditional;
+    
+    protected $mandtory;
+    protected $repeatable;
+    protected $language;
+    protected $errorMessage = 'standard_error';
+    protected $errorRegularExp = null;
+    
+    private function _translateArray($array) {
+        foreach ($array as $number => $value) {
+            $array[$number] = $translate->_($value);
+        }
+        return $array;
+    }
+    
+    public function Field($type, $mandatory = false, $repeatable = false, $language = false, $data = null) {
+        $this->type = $type;
+        $this->mandatory = $mandatory;
+        $this->repeatable = $repeatable;
+        $this->language = $language;
+        $this->value = $data['value'];
+        $this->valueLanguage = $data['language'];
+        $this->valueAdditional = $data['additional'];
+        
+        foreach ((array) $this->value as $number => $value) {
+            if ($value == '') {
+                unset($this->value[$number]);
+                unset($this->valueLanguage[$number]);
+                unset($this->valueAdditional[$number]);
+            }
+        }    
+    }
+    
+    public function isValid() {
+        if (!is_null($this->errorRegularExp)) {
+            foreach ($this->value as $valueLine) {
+                if (!mb_ereg_match($this->errorRegularExp, $valueLine)) 
+                    return false;
+            }       
+        }
+        return true;
+    }
+    
+    public function getValues() {
+        return array_values($this->value);
+    }
+    
+    public function getValuesLanguage() {
+        return _translateArray(array_values($this->valueLanguage));
+    }
+    
+    public function getValuesAdditional() {
+        return _translateArray(array_values($this->valueAdditional));
+    }
+    
+    public function getFieldType() {
+        return $this->type;
+    }
+    
+    public function getName() {
+        $translate->_($this->type.'_name');
+    }
+    
+    public function getErrorMessage() {
+        return $translate->_($this->errorMessage);
+    }
+}
+?>
