67cd77ba613029a56cadae4be4dde2f80cf286ff
Tobias Leidinger <tobias.leidinger@gmail.com>
2008-08-26 13:56:40 +0000

    worked on opus_form
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@613 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- library/Opus/Form/Field.php
+++ library/Opus/Form/Field.php
@@ -17,25 +17,29 @@ class Field {
     protected $language;
     protected $errorMessage = 'standard_error';
     protected $errorRegularExp = null;
+    protected $add = false;
     private $translate;
     
     private function _translateArray($array) {
-        foreach ($array as $number => $value) {
+        /*foreach ($array as $number => $value) {
             $array[$number] = $this->translate->_($value);
-        }
+        }*/
         return $array;
     }
     
     public function Field($type, $mandatory = false, $repeatable = false, $language = false, $data = null) {
-        $this->translate = Zend_Registry::getInstance()->get('Zend_Translate');
+        //$this->translate = Zend_Registry::getInstance()->get('Zend_Translate');
         
         $this->type = $type;
         $this->mandatory = $mandatory;
         $this->repeatable = $repeatable;
         $this->language = $language;
-        $this->value = $data['value'];
-        $this->valueLanguage = $data['language'];
-        $this->valueAdditional = $data['additional'];
+        $this->value = isset($data['value'])?$data['value']:array();
+        $this->valueLanguage = isset($data['language'])?$data['language']:array();
+        @$this->valueAdditional = isset($data['additional'])?$data['additional']:array();
+        if (isset($data['add'])) {
+            $this->setAdd();
+        }
         
         foreach ((array) $this->value as $number => $value) {
             if ($value == '') {
@@ -46,7 +50,18 @@ class Field {
         }    
     }
     
+    public function setAdd() {
+        $this->add = true;
+    }
+    
+    public function addLine() {
+        return $this->add;
+    }
+    
     public function isValid() {
+        if ($mandatory && count($this->value) == 0) {
+            return false;
+        }
         if (!is_null($this->errorRegularExp)) {
             foreach ($this->value as $valueLine) {
                 if (!mb_ereg_match($this->errorRegularExp, $valueLine)) 
@@ -61,11 +76,11 @@ class Field {
     }
     
     public function getValuesLanguage() {
-        return _translateArray(array_values($this->valueLanguage));
+        return array_values($this->valueLanguage);
     }
     
     public function getValuesAdditional() {
-        return _translateArray(array_values($this->valueAdditional));
+        return $this->valueAdditional;
     }
     
     public function getFieldType() {
@@ -73,15 +88,30 @@ class Field {
     }
     
     public function getName() {
-        $this->translate->_($this->type.'_name');
+        return /*$this->translate->_(*/$this->type.'_name';//);
     }
     
     public function isMandatory() {
         return $this->mandatory;
     }
     
+    public function isRepeatable() {
+        return $this->repeatable;
+    }
+    
+    public function hasLanguage() {
+        return $this->language;
+    }
+    
     public function getErrorMessage() {
-        return $this->translate->_($this->errorMessage);
+        return /*$this->translate->_(*/$this->errorMessage;//);
+    }
+    
+    public function render() {
+        $view = new Zend_View();
+        $view->addScriptPath('../library/Opus/Form');
+        $view->field = $this;
+        return $view->render('form_element.phtml');
     }
 }
 ?>
