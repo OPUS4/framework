1f516763a4ea88c1bd322edc18dc5bad98cbe97f
Tobias Leidinger <tobias.leidinger@gmail.com>
2008-09-09 10:50:30 +0000

    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@689 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/Opus/Document/StorageTest.php
+++ tests/Opus/Document/StorageTest.php
@@ -52,9 +52,10 @@ class Opus_Document_StorageTest extends PHPUnit_Framework_TestCase {
         /* $registry = Zend_Registry::getInstance();
          $adapter = $registry->get('db_adapter');
          $adapter->deleteTable('documents');*/
+        TestHelper::clearTable('document_title_abstracts');
         TestHelper::clearTable('documents');
         TestHelper::clearTable('licences');
-
+        
     }
 
     /**
@@ -71,9 +72,10 @@ class Opus_Document_StorageTest extends PHPUnit_Framework_TestCase {
      * @return void
      */
     public function testAddToDocuments() {
-        $this->fail('Opus_Db_Licences is not defined yet.');
+        //$this->fail('Opus_Db_Licences is not defined yet.');
         $licences = new Opus_Db_Licences();
         $licencesId = $licences->insert(array('name_long' => 'test licence'));
+        //TODO assertions for licence
 
         $data =
         array(
@@ -109,56 +111,63 @@ class Opus_Document_StorageTest extends PHPUnit_Framework_TestCase {
                 'volume' => '2008'
                 );
 
-                $storage = new Opus_Document_Storage($data);
-                $id = $storage->saveDocumentData();
-                $documents = new Opus_Db_Documents();
-                $rowSet = $documents->find($id);
-                $this->assertTrue($rowSet instanceof Zend_Db_Table_Rowset_Abstract);
-                $this->assertTrue($rowSet->count() == 1, 'Expected 1 dataset in table');
-                $row = $rowSet->current();
-                $rowData = $row->toArray();
-                $this->assertEquals($rowData['licences_id'], $licencesId);
-                $this->assertEquals($rowData['range_id'], '123');
-                $this->assertContains('2008-01-01', $rowData['completed_date']);
-                $this->assertEquals($rowData['completed_year'], '2008');
-                $this->assertEquals($rowData['contributing_corporation'], 'test corporation');
-                $this->assertEquals($rowData['creating_corporation'], 'test corporation');
-                $this->assertContains('2008-01-01', $rowData['date_accepted']);
-                $this->assertEquals($rowData['document_type'], 'article');
-                $this->assertEquals($rowData['edition'], '1');
-                $this->assertEquals($rowData['issue'], '1/2008');
-                $this->assertEquals($rowData['language'], 'de');
-                $this->assertEquals($rowData['non_institute_affiliation'], 'foreign institute');
-                $this->assertEquals($rowData['page_first'], '1');
-                $this->assertEquals($rowData['page_last'], '100');
-                $this->assertEquals($rowData['page_number'], '100');
-                $this->assertEquals($rowData['publication_status'], '1');
-                $this->assertContains('2008-01-01', $rowData['published_date']);
-                $this->assertEquals($rowData['published_year'], '2008');
-                $this->assertEquals($rowData['publisher_name'], 'test publisher');
-                $this->assertEquals($rowData['publisher_place'], 'Saarbrücken');
-                $this->assertEquals($rowData['publisher_university'], '1');
-                $this->assertEquals($rowData['reviewed'], 'open');
-                $this->assertContains('2008-01-01', $rowData['server_date_modified']);
-                $this->assertContains('2008-01-01', $rowData['server_date_published']);
-                $this->assertContains('2008-01-01', $rowData['server_date_unlocking']);
-                $this->assertContains('2008-01-01', $rowData['server_date_valid']);
-                $this->assertEquals($rowData['source'], 'teststring');
-                $this->assertEquals($rowData['swb_id'], '12345');
-                $this->assertEquals($rowData['vg_wort_pixel_url'], 'vg_wort_uri');
-                $this->assertEquals($rowData['volume'], '2008');
-                 
-                 
-
+        $storage = new Opus_Document_Storage($data);
+        $id = $storage->saveDocumentData();
+        $documents = new Opus_Db_Documents();
+        $rowSet = $documents->find($id);
+        $this->assertTrue($rowSet instanceof Zend_Db_Table_Rowset_Abstract);
+        $this->assertTrue($rowSet->count() == 1, 'Expected 1 dataset in table');
+        $row = $rowSet->current();
+        $rowData = $row->toArray();
+        $this->assertEquals($rowData['licences_id'], $licencesId);
+        $this->assertEquals($rowData['range_id'], '123');
+        $this->assertContains('2008-01-01', $rowData['completed_date']);
+        $this->assertEquals($rowData['completed_year'], '2008');
+        $this->assertEquals($rowData['contributing_corporation'], 'test corporation');
+        $this->assertEquals($rowData['creating_corporation'], 'test corporation');
+        $this->assertContains('2008-01-01', $rowData['date_accepted']);
+        $this->assertEquals($rowData['document_type'], 'article');
+        $this->assertEquals($rowData['edition'], '1');
+        $this->assertEquals($rowData['issue'], '1/2008');
+        $this->assertEquals($rowData['language'], 'de');
+        $this->assertEquals($rowData['non_institute_affiliation'], 'foreign institute');
+        $this->assertEquals($rowData['page_first'], '1');
+        $this->assertEquals($rowData['page_last'], '100');
+        $this->assertEquals($rowData['page_number'], '100');
+        $this->assertEquals($rowData['publication_status'], '1');
+        $this->assertContains('2008-01-01', $rowData['published_date']);
+        $this->assertEquals($rowData['published_year'], '2008');
+        $this->assertEquals($rowData['publisher_name'], 'test publisher');
+        $this->assertEquals($rowData['publisher_place'], 'Saarbrücken');
+        $this->assertEquals($rowData['publisher_university'], '1');
+        $this->assertEquals($rowData['reviewed'], 'open');
+        $this->assertContains('2008-01-01', $rowData['server_date_modified']);
+        $this->assertContains('2008-01-01', $rowData['server_date_published']);
+        $this->assertContains('2008-01-01', $rowData['server_date_unlocking']);
+        $this->assertContains('2008-01-01', $rowData['server_date_valid']);
+        $this->assertEquals($rowData['source'], 'teststring');
+        $this->assertEquals($rowData['swb_id'], '12345');
+        $this->assertEquals($rowData['vg_wort_pixel_url'], 'vg_wort_uri');
+        $this->assertEquals($rowData['volume'], '2008');
     }
-
-
-    /**
-     * Mock test.
-     *
-     * @return void
-     */
-    public function testSomething() {
-        $this->markTestIncomplete('Not implemented yet.');
+    
+    public function testAddTitle() {
+        $data =
+        array(
+                'document_type' => 'article',
+                array('title_abstract_type' => 'main', 'title_abstract_value' => 'main title', 'title_abstract_language' => 'de')
+        );
+        $storage = new Opus_Document_Storage($data);
+        $id = $storage->saveDocumentData();
+        
+        $title_abstract = new Opus_Db_DocumentTitleAbstracts();
+        $where = $title_abstract->getAdapter()->quoteInto('documents_id = ?', $id);
+        $row = $title_abstract->fetchRow($where);
+        $this->assertTrue($row instanceof Zend_Db_Table_Row_Abstract, 'Object of type Zend_Db_Table_Row_Abstract expected');
+        $this->assertEquals($row->title_abstract_type, 'main');
+        $this->assertEquals($row->title_abstract_value, 'main title');
+        $this->assertEquals($row->title_abstract_language, 'de');
     }
+    
+    
 }
