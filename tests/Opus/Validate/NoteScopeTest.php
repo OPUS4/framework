8dd637e602c675d3ebb23877d00848a5cb6f8869
Ralf Claußnitzer <ralf.claussnitzer@slub-dresden.de>
2008-09-12 14:03:39 +0000

    Set svn properties.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@718 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/Opus/Validate/NoteScopeTest.php
+++ tests/Opus/Validate/NoteScopeTest.php
@@ -29,7 +29,7 @@
  * @author      Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
  * @copyright   Copyright (c) 2008, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
- * @version     $Id: TypeTest.php 711 2008-09-12 12:08:05Z claussnitzer $
+ * @version     $Id$
  */
 
 
