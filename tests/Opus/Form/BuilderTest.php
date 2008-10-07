c01612d665dd03b4b7a0463ae137dcf2a4f89cba
Henning Gerhardt <henning.gerhardt@slub-dresden.de>
2008-10-07 13:03:59 +0000

    Add initial tests for Opus_Form_Builder.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@783 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/Opus/Form/BuilderTest.php
+++ tests/Opus/Form/BuilderTest.php
@@ -26,58 +26,38 @@
  *
  * @category    Tests
  * @package     Opus_Form
- * @author      Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
+ * @author      Ralf Claußnitzer <ralf.claussnitzer@slub-dresden.de>
+ * @author      Henning Gerhardt <henning.gerhardt@slub-dresden.de>
  * @copyright   Copyright (c) 2008, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  * @version     $Id$
  */
 
-// The phpunit testrunner defines the global PHPUnit_MAIN_METHOD to
-// configure the method of test execution. When called via php directly
-// PHPUnit_MAIN_METHOD is not defined and therefor gets defined to execute
-// AllTests:main() to run the suite.
-if ( defined('PHPUnit_MAIN_METHOD') === false ) {
-    define('PHPUnit_MAIN_METHOD', 'Opus_Form_AllTests::main');
-}
-
-// Use the TestHelper to setup Zend specific environment.
-require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
-
 /**
- * Main test suite for testing database access and models.
+ * Test cases for class Opus_Form_Builder.
  *
- * @category    Tests
- * @package     Opus_Form
+ * @category Tests
+ * @package  Opus_Form
+ *
+ * @group    BuilderTest
  */
-class Opus_Form_AllTests {
+class Opus_Form_BuilderTest extends PHPUnit_Framework_TestCase {
 
     /**
-     * If the test class is called directly via php command the test
-     * run gets startet in this method.
+     * Test of creating a Zend Form.
      *
      * @return void
      */
-    public static function main() {
-        PHPUnit_TextUI_TestRunner::run(self::suite());
+    public function testCreateForm() {
+        $this->markTestIncomplete('Not implemented');
     }
 
     /**
-     * Construct and return the test suite.
+     * Tries to recreate a form.
      *
-     * WARNING: <b>This will drop and recreate the whole database.</b>
-     *
-     * @return PHPUnit_Framework_TestSuite The suite.
+     * @return void
      */
-    public static function suite() {
-        $suite = new PHPUnit_Framework_TestSuite('Opus Application Framework - Opus_Form');
-        $suite->addTestSuite('Opus_Form_BuilderTest');
-        $suite->addTestSuite('Opus_Form_LayoutTest');
-        return $suite;
+    public function testRecreateForm() {
+        $this->markTestIncomplete('Not implemented');
     }
-
-}
-
-// Execute the test run if necessary.
-if (PHPUnit_MAIN_METHOD === 'Opus_Form_AllTests::main') {
-    Opus_Form_AllTests::main();
 }
