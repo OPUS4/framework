8315e9e505e880169d1ca6d922fd785ee862540a
Oliver Marahrens <o.marahrens@tu-harburg.de>
2008-12-05 07:56:10 +0000

    added converters to get plain text for the Lucene fulltext index from files, added OAIReaderclass for importer
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@1148 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ library/Opus/Search/Index/FileFormatConverter/FileFormatConverterInterface.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Interface that defines structure for FileFormatConversion-Classes
+ * 
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    FileFormatConverter
+ * @package     Module_Search
+ * @author      Oliver Marahrens (o.marahrens@tu-harburg.de)
+ * @copyright   Copyright (c) 2008, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+interface Opus_Search_Index_FileFormatConverter_FileFormatConverterInterface
+{
+  /**
+   * converts a file to plain text
+   * @return String fulltext
+   * @param String $filepath path to the file that should be converted to text
+   * @access public
+   */
+	public static function toText($filepath);
+}
