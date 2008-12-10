8abac77b076c9fa10c120dafc1f8f1d63b5f5909
Felix Ostrowski <ostrowski@hbz-nrw.de>
2008-12-10 16:16:09 +0000

    Adds Opus_Dependent_Link model for authors and licences.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@1244 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ library/Opus/Model/Dependent/Link/Author.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Framework
+ * @package     Opus_Model
+ * @author      Felix Ostrowski (ostrowski@hbz-nrw.de)
+ * @copyright   Copyright (c) 2008, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * Abstract class for link author model in the Opus framework.
+ *
+ * @category    Framework
+ * @package     Opus_Model
+ */
+class Opus_Model_Dependent_Link_Author extends Opus_Model_Dependent_Link_Abstract
+{
+    /**
+     * Primary key of the parent model.
+     *
+     * @var mixed $_parentId.
+     */
+    protected $_parentColumn = 'documents_id';
+
+    /**
+     * Create a new parent title model instance.
+     *
+     * @see Opus_Model_Abstract::__construct()
+     * @param mixed $id (Optional) Primary key of a persisted title model instance.
+     * @param mixed $parent_id (Optional) Primary key of the parent document.
+     * @param Zend_Db_Table $tableGatewayModel
+     * @throws Opus_Model_Exception Thrown if an instance with the given primary key could not be found.
+     */
+    public function __construct($id = null, $tableGatewayModel = null) {
+        if ($tableGatewayModel === null) {
+            parent::__construct($id, new Opus_Db_LinkDocumentsPersons);
+        } else {
+            parent::__construct($id, $tableGatewayModel);
+        }
+    }
+
+    /**
+     * Initialize model with the following values:
+     * - Role
+     *
+     * @return void
+     */
+    protected function _init() {
+        if (is_null($this->getId()) === false) {
+            $this->_model = new Opus_Model_Person($this->_primaryTableRow->persons_id);
+        } else {
+            $this->_model = new Opus_Model_Person();
+        }
+        $this->_primaryTableRow->role = 'author';
+    }
+
+    /**
+     * Persist foreign model & link.
+     *
+     * @return void
+     */
+    public function store() {
+        $this->_model->_setTransactional(false);
+        $this->_primaryTableRow->institutes_id = 0;
+        $this->_primaryTableRow->persons_id = $this->_model->store();
+        parent::store();
+    }
+
+}
+
