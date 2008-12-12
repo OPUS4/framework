d0e236a979e3231ca1cc8d35e558cc9f57f608b5
Ralf Claußnitzer <ralf.claussnitzer@slub-dresden.de>
2008-12-12 14:15:45 +0000

    Fix: Missing constructor argument for Opus_Validate_PublishedYear.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@1281 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- library/Opus/Validate/PublishedYear.php
+++ library/Opus/Validate/PublishedYear.php
@@ -41,10 +41,11 @@
 class Opus_Validate_PublishedYear extends Zend_Validate_GreaterThan  {
     
     /**
-     * The smallest valid year is 1900.
-     *
-     * @var Int
+     * Set the smallest valid year to 1900.
+     * 
      */
-    protected $_min = 1900;
+    public function __construct() {
+        parent::__construct(1900);
+    }
     
 }
