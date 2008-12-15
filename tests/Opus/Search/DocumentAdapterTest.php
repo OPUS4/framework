27856a608163ce984399b8366e7f58d87897dfce
Oliver Marahrens <o.marahrens@tu-harburg.de>
2008-12-15 09:37:59 +0000

    adapted test to new db schema
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@1302 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/Opus/Search/DocumentAdapterTest.php
+++ tests/Opus/Search/DocumentAdapterTest.php
@@ -45,14 +45,14 @@ class Opus_Search_DocumentAdapterTest extends PHPUnit_Framework_TestCase {
         $adapter->query("DELETE FROM `document_title_abstracts` WHERE `documents_id` = 37");
         $adapter->query("DELETE FROM `link_persons_documents` WHERE `documents_id` = 37");
         $adapter->query("DELETE FROM `persons` WHERE `persons_id` = 1");
-        $adapter->query("INSERT INTO `documents` (`documents_id`, `range_id`, `completed_date`, `completed_year`, `contributing_corporation`, `creating_corporation`, `date_accepted`, `document_type`, `edition`, `issue`, `language`, `non_institute_affiliation`, `page_first`, `page_last`, `page_number`, `publication_state`, `published_date`, `published_year`, `publisher_name`, `publisher_place`, `publisher_university`, `reviewed`, `server_date_modified`, `server_date_published`, `server_date_unlocking`, `server_date_valid`, `source`, `swb_id`, `vg_wort_pixel_url`, `volume`) VALUES
-(37, NULL, NULL, 0000, NULL, NULL, NULL, 'monograph', NULL, NULL, 'ger', NULL, NULL, NULL, NULL, 0, NULL, 2002, 'Universitätsbibliothek der Technischen Universität Hamburg-Harburg', 'Hamburg-Harburg', 0, 'peer', NULL, '0000-00-00 00:00:00', NULL, NULL, NULL, NULL, NULL, NULL)");
+        $adapter->query("INSERT INTO `documents` (`documents_id`, `range_id`, `completed_date`, `completed_year`, `contributing_corporation`, `creating_corporation`, `date_accepted`, `document_type`, `edition`, `issue`, `language`, `non_institute_affiliation`, `page_first`, `page_last`, `page_number`, `publication_state`, `published_date`, `published_year`, `reviewed`, `server_date_modified`, `server_date_published`, `server_date_unlocking`, `server_date_valid`, `source`, `swb_id`, `vg_wort_pixel_url`, `volume`) VALUES
+(37, NULL, NULL, 0000, NULL, NULL, NULL, 'monograph', NULL, NULL, 'ger', NULL, NULL, NULL, NULL, 0, NULL, 2002, 'peer', NULL, '0000-00-00 00:00:00', NULL, NULL, NULL, NULL, NULL, NULL)");
         $adapter->query("INSERT INTO `document_title_abstracts` (`document_title_abstracts_id`, `documents_id`, `title_abstract_type`, `title_abstract_value`, `title_abstract_language`) VALUES
 (1, 37, 'main', 'Informationskompetenz und studentisches Lernen im elektronischen Zeitalter', 'ger'),
 (2, 37, 'main', 'Information literacy and student learning in the electronic age', 'eng'),
 (3, 37, 'abstract', 'Die Integration der Vermittlung von allgemeiner und fachlicher\nInformationskompetenz in das Lernen und Lehren an unseren Universitäten ist\neine wichtige Voraussetzung für die zeitgemässe Qualifizierung von\nHochschulabsolventen. Der Beitrag gibt eine Übersicht zur Ausgangssituation, zu\nProblemen, Zielen, Inhalten und Methoden der Vermittlung von\nInformationskompetenz im Rahmen studentischen Lernens aus der Sicht der\nUniversitätsbibliothek einer Technischen Universität.', 'ger')");
-        $adapter->query("INSERT INTO `link_documents_persons` (`link_documents_persons_id`, `documents_id`, `persons_id`, `institutes_id`, `role`, `sort_order`) VALUES
-(1, 37, 1, 1, 'author', 0)");
+        $adapter->query("INSERT INTO `link_documents_persons` (`persons_id`, `documents_id`, `institutes_id`, `role`, `sort_order`) VALUES
+(1, 37, 1, 'author', 0)");
 		$adapter->query("INSERT INTO `persons` (`persons_id`, `academic_title`, `date_of_birth`, `email`, `first_name`, `last_name`, `place_of_birth`) VALUES
 (1, '', '2008-07-15 00:00:00', 'mustermann@domain.com', 'Thomas', 'Hapke', 'Musterstadt')");
     }
