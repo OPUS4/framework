7c2586c8b2f36113e7f3cbda2d556bf30fdac1bf
Henning Gerhardt <henning.gerhardt@slub-dresden.de>
2009-01-23 12:08:07 +0000

    Reworked helper method to work with array data.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@1601 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- library/Opus/View/Helper/ShowModel.php
+++ library/Opus/View/Helper/ShowModel.php
@@ -41,163 +41,144 @@
  */
 class Opus_View_Helper_ShowModel extends Zend_View_Helper_Abstract {
 
-    private function __skeleton(Opus_Model_Field $field, $label = null, $value = null) {
+    private function __skeleton($name, $value, $label = null) {
         $result = array();
-        $result['divclass'] = $field->getName();
-        $result['labelclass'] = $field->getName() . ' label';
-        $result['valueclass'] = $field->getName() . ' value';
-        $result['label'] = $label;
+        $result['divclass'] = $name;
+        $result['labelclass'] = $name . ' label';
+        $result['valueclass'] = $name . ' value';
+        if (empty($label) === true) {
+            $result['label'] = $name;
+        } else {
+            $result['label'] = $label;
+        }
         $result['value'] = $value;
         return $result;
     }
 
-    private function __personHelper(Opus_Model_Interface $model, $fieldname) {
-        $field = $model->getField($fieldname);
-        $label = $field->getName();
-        $value = $field->getValue();
-        $iterim = $this->__skeleton($field, $label, $value);
-        return $iterim;
-    }
-
-    private function __personDisplay(Opus_Model_Field $field) {
-        $value = $field->getValue();
-        $result = '';
-        // only one element to display
-        if (is_object($value) === true) {
-            $label = $field->getName();
-            $model = $field->getValue();
-            $data = array();
-            foreach ($value->describe() as $fieldname) {
-                $data[] = $this->__personHelper($model, $fieldname);
-            }
-            $iterim_value = $this->view->partialLoop('_model.phtml', $data);
-            $outer = $this->__skeleton($field, $label, $iterim_value);
-            $result = $this->view->partial('_model.phtml', $outer);
-        } if (is_array($value) === true) {
-            // more than one element to display
-            foreach ($value as $number => $model) {
-                $data = array();
-                $label = ++$number . '.' . $field->getName();
-                foreach($model->describe() as $fieldname)  {
-                    $data[] = $this->__personHelper($model, $fieldname);
-                }
-                $iterim_value = $this->view->partialLoop('_model.phtml', $data);
-                $outer = $this->__skeleton($field, $label, $iterim_value);
-                $result .= $this->view->partial('_model.phtml', $outer);
-            }
-        }
-        return $result;
-    }
-
-    private function __titleHelper(Opus_Model_Interface $model) {
+    private function __personHelper($field, &$value, $label = null) {
         $data = array();
-        $language_list = Zend_Registry::get('Available_Languages');
-        // set language field
-        $field_titleLanguage = $model->getField('TitleAbstractLanguage');
-        $label = $field_titleLanguage->getName();
-        $value = $language_list[$field_titleLanguage->getValue()];
-        $titleLanguage = $this->__skeleton($field_titleLanguage, $label, $value);
-        $data[] = $titleLanguage;
-        // set value field
-        $field_titleValue = $model->getField('TitleAbstractValue');
-        $label = $field_titleValue->getName();
-        $value = $field_titleValue->getValue();
-        $titleValue = $this->__skeleton($field_titleValue, $label, $value);
-        $data[] = $titleValue;
-        return $data;
+        foreach ($value as $fieldname => $internal_value) {
+            $data[] = $this->__skeleton($fieldname, $internal_value);
+        }
+        $iterim_data = $this->view->partialLoop('_model.phtml', $data);
+        $outer = $this->__skeleton($field, $iterim_data, $label);
+        return $this->view->partial('_model.phtml', $outer);
     }
 
-    private function __titleDisplay(Opus_Model_Field $field) {
-        $value = $field->getValue();
+    private function __personDisplay($field, &$values) {
+        // silence decision about multi values or not
         $result = '';
-        // only one element to display
-        if (is_object($value) === true) {
-            $value = $field->getValue();
-            $label = $field->getName();
-            $data = $this->__titleHelper($value);
-            $iterim_value = $this->view->partialLoop('_model.phtml', $data);
-            $outer = $this->__skeleton($field, $label, $iterim_value);
-            $result = $this->view->partial('_model.phtml', $outer);
-        } if (is_array($value) === true) {
+        if (@is_array($values[0]) === false) {
+            // only one element to display
+            $result = $this->__personHelper($field, $values);
+        } else {
             // more than one element to display
-            foreach ($value as $number => $model) {
-                $label = ++$number . '.' . $field->getName();
-                $iterim_data = $this->__titleHelper($model);
-                $iterim_value = $this->view->partialLoop('_model.phtml', $iterim_data);
-                $outer = $this->__skeleton($field, $label, $iterim_value);
-                $result .= $this->view->partial('_model.phtml', $outer);
+            foreach ($values as $number => $value) {
+                $label = ++$number . '. ' . $field;
+                $result .= $this->__personHelper($field, $value, $label);
             }
         }
         return $result;
     }
 
-    protected function _displayGeneralElement(Opus_Model_Field $field) {
-        $label = $field->getName();
-        $value = $field->getValue();
-        $data = $this->__skeleton($field, $label, $value);
-        return $this->view->partial('_model.phtml', $data);
-    }
-
-    protected function _displayLicence(Opus_Model_Field $field) {
-        $label = $field->getName();
-        $value = $field->getValue();
-        $value = $value->getField('NameLong')->getValue();
-        $data = $this->__skeleton($field, $label, $value);
-        return $this->view->partial('_model.phtml', $data);
-    }
-
-    protected function _displayLanguage(Opus_Model_Field $field) {
+    private function __titleHelper($field, &$value, $label = null) {
+        $data = array();
+        // title language
         $language_list = Zend_Registry::get('Available_Languages');
-        $label = $field->getName();
-        $value = $language_list[$field->getValue()];
-        $data = $this->__skeleton($field, $label, $value);
-        return $this->view->partial('_model.phtml', $data);
+        $language_field = 'TitleAbstractLanguage';
+        $language = $language_list[$value[$language_field]];
+        $data[] = $this->__skeleton($language_field, $language);
+        // title value
+        $title_field = 'TitleAbstractValue';
+        $iterim_value = $value[$title_field];
+        $data[] = $this->__skeleton($title_field, $iterim_value);
+        $iterim_data = $this->view->partialLoop('_model.phtml', $data);
+        $outer = $this->__skeleton($field, $iterim_data, $label);
+        return $this->view->partial('_model.phtml', $outer);
     }
 
-    protected function _displayPersonAdvisor(Opus_Model_Field $field) {
-        return $this->__personDisplay($field);
-    }
-
-    protected function _displayPersonAuthor(Opus_Model_Field $field) {
-        return $this->__personDisplay($field);
-    }
-
-    protected function _displayPersonReferee(Opus_Model_Field $field) {
-        return $this->__personDisplay($field);
-    }
-
-    protected function _displayPersonOther(Opus_Model_Field $field) {
-        return $this->__personDisplay($field);
-    }
-
-    protected function _displayIsbn(Opus_Model_Field $field) {
-        return $this->__personDisplay($field);
-    }
-
-    protected function _displayTitleAbstract(Opus_Model_Field $field) {
-        return $this->__titleDisplay($field);
-    }
-
-    protected function _displayTitleMain(Opus_Model_Field $field) {
-        return $this->__titleDisplay($field);
-    }
-
-    protected function _displayTitleParent(Opus_Model_Field $field) {
-        return $this->__titleDisplay($field);
-    }
-
-    public function showModel(Opus_Model_Interface $model) {
-        $return = '';
-        foreach ($model->describe() as $fieldname) {
-            $field = $model->getField($fieldname);
-            $method_name = '_display' . $fieldname;
-            if (method_exists($this, $method_name) === true) {
-                $return .= $this->$method_name($field);
-            } else {
-                $return .= $this->_displayGeneralElement($field);
+    private function __titleDisplay($field, $values) {
+        $result = '';
+        if (@is_array($values[0]) === false) {
+            // only one element to display
+            $result = $this->__titleHelper($field, $values);
+        } else {
+            // more than one element to display
+            foreach ($values as $number => $value) {
+                $label = ++$number . '. '. $field;
+                $result .= $this->__titleHelper($field, $value, $label);
             }
         }
-        return $return;
+        return $result;
+    }
+
+    protected function _displayGeneralElement($name, $value) {
+        $data = $this->__skeleton($name, $value);
+        return $this->view->partial('_model.phtml', $data);
+    }
+
+    protected function _displayLicence($field, $value) {
+        // we "know" that the licence name is in NameLong
+        $iterim_value = $value['NameLong'];
+        $data = $this->__skeleton($field, $iterim_value);
+        return $this->view->partial('_model.phtml', $data);
+    }
+
+    protected function _displayLanguage($field, $value) {
+        $language_list = Zend_Registry::get('Available_Languages');
+        $iterim_value = $language_list[$value];
+        $data = $this->__skeleton($field, $iterim_value);
+        return $this->view->partial('_model.phtml', $data);
+    }
+
+    protected function _displayFile($field, $value) {
+        // TODO need more information for displaying
+        return;
+    }
+
+    protected function _displayPersonAdvisor($field, $value) {
+        return $this->__personDisplay($field, $value);
+    }
+
+    protected function _displayPersonAuthor($field, $value) {
+        return $this->__personDisplay($field, $value);
+    }
+
+    protected function _displayPersonReferee($field, $value) {
+        return $this->__personDisplay($field, $value);
+    }
+
+    protected function _displayPersonOther($field, $value) {
+        return $this->__personDisplay($field, $value);
+    }
+
+    protected function _displayIsbn($field, $value) {
+        return $this->__personDisplay($field, $value);
+    }
+
+    protected function _displayTitleAbstract($field, $value) {
+        return $this->__titleDisplay($field, $value);
+    }
+
+    protected function _displayTitleMain($field, $value) {
+        return $this->__titleDisplay($field, $value);
+    }
+
+    protected function _displayTitleParent($field, $value) {
+        return $this->__titleDisplay($field, $value);
+    }
+
+    public function showModel(array $modeldata) {
+        $result = '';
+        foreach ($modeldata as $field => $value) {
+            $method_name = '_display' . $field;
+            if (method_exists($this, $method_name) === true) {
+                $result .= $this->$method_name($field, $value);
+            } else {
+                $result .= $this->_displayGeneralElement($field, $value);
+            }
+        }
+        return $result;
     }
 
 }
