192ee21d2ff1a7e5e43ccb442f100fe7f8c89e16
Felix Ostrowski <ostrowski@hbz-nrw.de>
2009-02-05 17:48:10 +0000

    Adds model for collection roles.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@1809 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ library/Opus/Model/Collection/Role.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the North Rhine-Westphalian Library Service Center,
+ * the Cooperative Library Network Berlin-Brandenburg, the Saarland University
+ * and State Library, the Saxon State Library - Dresden State and University
+ * Library, the Bielefeld University Library and the University Library of
+ * Hamburg University of Technology with funding from the German Research
+ * Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License 
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51 
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Framework
+ * @package     Opus_Model
+ * @author      Felix Ostrowski <ostrowski@hbz-nrw.de>
+ * @copyright   Copyright (c) 2009, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * Domain model for collection roles in the Opus framework
+ *
+ * @category    Framework
+ * @package     Opus_Model
+ * @uses        Opus_Model_Abstract
+ */
+class Opus_Model_Collection_Role extends Opus_Model_Abstract {
+
+    /**
+     * Specify then table gateway.
+     *
+     * @var string Classname of Zend_DB_Table to use if not set in constructor.
+     */
+    protected static $_tableGatewayClass = 'Opus_Db_CollectionsRoles';
+
+    /**
+     * Initialize model with the following fields:
+     * - Name
+     * - Position
+     * - LinkDocsPathToRoot
+     * - Visible
+     *
+     * @return void
+     */
+    protected function _init() {
+        $name = new Opus_Model_Field('Name');
+        $position = new Opus_Model_Field('Position');
+        $links_docs_path_to_root = new Opus_Model_Field('LinkDocsPathToRoot');
+        $visible = new Opus_Model_Field('Visible');
+
+        $this->addField($name)
+            ->addField($position)
+            ->addField($links_docs_path_to_root)
+            ->addField($visible);
+    }
+
+    /**
+     * Returns long name.
+     *
+     * @see library/Opus/Model/Opus_Model_Abstract#getDisplayName()
+     */
+    public function getDisplayName() {
+       return $this->getName();
+    }
+
+    /**
+     * Retrieve all Opus_Model_Collection_Role instances from the database.
+     *
+     * @return array Array of Opus_Model_Collection_Role objects.
+     */
+    public static function getAll() {
+        return self::getAllFrom('Opus_Model_Collection_Role', 'Opus_Db_CollectionsRoles');
+    }
+}
