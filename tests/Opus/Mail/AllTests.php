708b9b25f2f55e80fdce8c054ee6b1fffc00a099
Eva Kranz <s9evkran@stud.uni-saarland.de>
2009-02-13 10:27:11 +0000

    PHP unit tests
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@1927 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/Opus/Mail/AllTests.php
+++ tests/Opus/Mail/AllTests.php
@@ -26,11 +26,12 @@
  *
  * @category    Tests
  * @package     Opus_Mail
- * @author      Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
- * @copyright   Copyright (c) 2008, OPUS 4 development team
+ * @author      Eva Kranz <eva@o-dyn.de>
+ * @copyright   Copyright (c) 2009, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  * @version     $Id$
  */
+
 
 // The phpunit testrunner defines the global PHPUnit_MAIN_METHOD to
 // configure the method of test execution. When called via php directly
@@ -70,9 +71,9 @@ class Opus_Mail_AllTests {
      */
     public static function suite() {
         $suite = new PHPUnit_Framework_TestSuite('Opus Application Framework - Opus_Mail');
+        $suite->addTestSuite('Opus_Document_SendMailTest');
         return $suite;
     }
-
 }
 
 // Execute the test run if necessary.
