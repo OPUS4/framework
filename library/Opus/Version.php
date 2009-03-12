19f7a1460d64cdbd581b509d0b206cbf2207d13e
Ralf Claußnitzer <ralf.claussnitzer@slub-dresden.de>
2009-03-12 09:15:40 +0000

    Add Opus_Version class for framework version comparing (see Zend_Version).
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@2153 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ library/Opus/Version.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Framework
+ * @package     Opus
+ * @author      Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
+ * @copyright   Copyright (c) 2008, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * Provide Opus Framework Version.
+ *
+ * @category    Framework
+ * @package     Opus
+ *
+ */
+class Opus_Version {
+    /*    
+     * Opus Framework version identification - see compareVersion()
+     */
+    const VERSION = '1.0.0';
+
+    /**
+     * Compare the specified Opus Framework version string $version
+     * with the current Opus_Version::VERSION of the Zend Framework.
+     *
+     * @param  string  $version  A version string (e.g. "0.7.1").
+     * @return integer           -1 if the $version is older,
+     *                           0 if they are the same,
+     *                           and +1 if $version is newer.
+     *
+     */
+    public static function compareVersion($version)
+    {
+        return version_compare($version, self::VERSION);
+    }
+}
+
