191485825a636377032a5fcc0fbde0d771a4eba6
Felix Ostrowski <ostrowski@hbz-nrw.de>
2009-05-13 07:45:23 +0000

    Fixes tunneling calls to filtered model.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@2717 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- library/Opus/Model/Filter.php
+++ library/Opus/Model/Filter.php
@@ -155,7 +155,13 @@ class Opus_Model_Filter extends Opus_Model_Abstract {
         if (in_array($fieldname, $this->blacklist)) {
             throw new Opus_Model_Exception('Requested field is hidden by the blacklist.');
         }
-        return $this->model->__call($name, $arguments);
+        foreach ($arguments as $i => $argument) {
+            if (true === is_string($argument)) {
+                $arguments[$i] = '\'' . $argument . '\'';
+            }
+        }
+        eval('$result = $this->model->$name('. implode($arguments, ',') . ');');
+        return $result;
     }
 
 
