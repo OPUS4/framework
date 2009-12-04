b2c6871853acaf28be95cdc359bbf476111a2b70
Felix Ostrowski <ostrowski@hbz-nrw.de>
2009-12-04 08:25:28 +0000

    Fixes link model for detailed person information.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@3894 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- library/Opus/Model/Dependent/Link/DocumentPersonLong.php
+++ library/Opus/Model/Dependent/Link/DocumentPersonLong.php
@@ -38,8 +38,14 @@
  * @category    Framework
  * @package     Opus_Model
  */
-class Opus_Model_Dependent_Link_DocumentPersonLong extends Opus_Model_Dependent_Link_DocumentPerson
+class Opus_Model_Dependent_Link_DocumentPersonLong extends Opus_Model_Dependent_Link_Abstract
 {
+    /**
+     * Primary key of the parent model.
+     *
+     * @var mixed $_parentId.
+     */
+    protected $_parentColumn = 'document_id';
 
     /**
      * The class of the model that is linked to.
@@ -48,4 +54,78 @@ class Opus_Model_Dependent_Link_DocumentPersonLong extends Opus_Model_Dependent_
      */
     protected $_modelClass = 'Opus_PersonLong';
 
+    /**
+     * Specify then table gateway.
+     *
+     * @var string Classname of Zend_DB_Table to use if not set in constructor.
+     */
+    protected static $_tableGatewayClass = 'Opus_Db_LinkPersonsDocuments';
+
+    /**
+     * Fields that should not be displayed on a form.
+     *
+     * @var array  Defaults to array('File').
+     */
+    protected $_internalFields = array(
+            'Role',
+            'SortOrder',
+        );
+
+
+    /**
+     * Initialize model with the following values:
+     * - Institute
+     * - Role
+     * - SortOrder
+     *
+     * @return void
+     */
+    protected function _init() {
+        if (is_null($this->getId()) === false) {
+            $this->setModel(new Opus_PersonLong($this->_primaryTableRow->person_id));
+        }
+
+        $institute = new Opus_Model_Field('InstituteId');
+        $institute->setSelection(true);
+
+        // Custom MySQL-Query for the sake of speed.
+        // We might want to consider something like
+        // this for collection drop-down lists.
+        $db = Zend_Registry::get('db_adapter');
+        $collections = array();
+        if (true === in_array('collections_structure_1', $db->listTables())) {
+            $query = "SELECT c.name, n.collections_id, COUNT(*)-1 AS level FROM collections_structure_1 AS n JOIN
+                collections_contents_1 AS c ON c.id = n.collections_id, collections_structure_1 AS p WHERE n.left BETWEEN
+                p.left AND p.right GROUP BY n.left ORDER BY n.left";
+            $collections = $db->query($query)->fetchAll();
+        }
+        $instDefaults = array();
+        foreach ($collections as $collection) {
+            $levelString = str_repeat('-', $collection['level']);
+            $instDefaults[$collection['collections_id']] = $levelString . ' ' . $collection['name'];
+        }
+        $institute->setDefault($instDefaults);
+
+        $role = new Opus_Model_Field('Role');
+        $sortOrder = new Opus_Model_Field('SortOrder');
+        $allowEmailContact = new Opus_Model_Field('AllowEmailContact');
+        $allowEmailContact->setCheckbox(true);
+
+        $this->addField($role)
+            ->addField($sortOrder)
+            ->addField($allowEmailContact)
+            ->addField($institute);
+    }
+
+    /**
+     * Persist foreign model & link.
+     *
+     * @return void
+     */
+    public function store() {
+        $this->_primaryTableRow->person_id = $this->_model->store();
+        parent::store();
+    }
+
 }
+
