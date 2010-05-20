52dbed6ceb9c980b8800a089fa5cd5cf2201d8c7
unknown <unknown>
2010-05-20 14:16:54 +0000

    Changed SQL dumps and scripts so that database name can be changed in createdb.sh and rebuilding_database.sh.
    fixes #546
    
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@4395 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/schema/opus400.sql
+++ db/schema/opus400.sql
@@ -2,11 +2,6 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `opus400` 
-DEFAULT CHARACTER SET = utf8 
-DEFAULT COLLATE = utf8_general_ci;
-USE `opus400`;
-
 -- -----------------------------------------------------
 -- Table to store schema versioning information
 -- -----------------------------------------------------
