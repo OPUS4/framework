6b402879cd99582802933dacc56f7941d543d9f3
Thoralf Klein <thoralf@fischlustig.de>
2010-08-11 15:04:26 +0000

    Added RequireTest, which tries to load all classes via require_once.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@4692 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/Opus/RequireTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Tests
+ * @package     Opus
+ * @author      Thoralf Klein <thoralf.klein@zib.de>
+ * @copyright   Copyright (c) 2010, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * Test cases to load all class files.
+ *
+ * @package Opus
+ * @category Tests
+ *
+ * @group RequireTest
+ *
+ */
+class Opus_RequireTest extends TestCase {
+
+    private $_classFiles = array();
+
+    /**
+     * Overwrite standard setUp method, no database connection needed.  Will
+     * create a file listing of class files instead.
+     *
+     * @return void
+     */
+    public function setUp() {
+        // Run find to locate all class files
+        $cmd = 'find ../library/Opus/ -type f -iname "*php" |cut -d/ -f3-';
+        // Result will be written to $this->_classFiles
+        exec($cmd, $this->_classFiles);
+    }
+
+    /**
+     * Overwrite standard tearDown method, no cleanup needed.
+     *
+     * @return void
+     */
+    public function tearDown() {
+    }
+
+    /**
+     * Try to load all class files, just to make sure no syntax error have
+     * been introduced.  As a side effect, all classes will be visible to
+     * code coverage report.
+     *
+     * @return void
+     */
+    public function testRequire() {
+        foreach ($this->_classFiles AS $file) {
+            require_once($file);
+        }
+    }
+}
