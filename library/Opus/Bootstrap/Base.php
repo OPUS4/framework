8d2e848c6f917cc0acb072b97516d2b3bee35b34
Jens Schwidder <schwidder@zib.de>
2010-08-31 08:39:27 +0000

    Exceptions bei der Initialisierung des Datenbankadapters werden jetzt gefangen. In *production* eine kurze Meldung wird ausgegeben und das Skript abgebrochen, ansonsten wird die Exception einfach weiter gereicht. Die Fehlerbehandlung im Bootstrap kann später noch weiter verfeinert werden.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@5097 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- library/Opus/Bootstrap/Base.php
+++ library/Opus/Bootstrap/Base.php
@@ -116,11 +116,25 @@ class Opus_Bootstrap_Base extends Zend_Application_Bootstrap_Bootstrap {
 
         // Use zend_Db factory to create a database adapter
         // and make it the default for all tables.
-        $db = Zend_Db::factory($config->db);
-        Zend_Db_Table::setDefaultAdapter($db);
+        $db = null;
 
-        // Register the adapter within Zend_Registry.
-        Zend_Registry::set('db_adapter', $db);
+        try {
+            $db = Zend_Db::factory($config->db);
+            Zend_Db_Table::setDefaultAdapter($db);
+
+            // Register the adapter within Zend_Registry.
+            Zend_Registry::set('db_adapter', $db);
+        }
+        catch (Zend_Db_Adapter_Exception $e) {
+            if ($this->getEnvironment() === "production") {
+                $logger->err($e);
+                echo "OPUS Bootstrap Error: Database not found!";
+                exit(-1);
+            }
+            else {
+                throw $e;
+            }
+        }
     }
 
     /**
