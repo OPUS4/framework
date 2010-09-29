da8ad4bd4a194bc1b5e0f20a117a2926505aeb1a
Pascal-Nicolas Becker <opus@pascal-becker.de>
2010-09-29 16:29:27 +0000

    Added default 0 for dnb_institutes.is_grantor.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@6049 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/schema/opus400.sql
+++ db/schema/opus400.sql
@@ -472,14 +472,14 @@ COMMENT = 'Table for identifiers referencing to related documents.';
 --
 CREATE  TABLE IF NOT EXISTS `languages` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `part2_b` char(3) default NULL COMMENT 'Equivalent 639-2 identifier of the bibliographic applications code set, if there is one',
-  `part2_t` char(3) default NULL COMMENT 'Equivalent 639-2 identifier of the terminology applications code set, if there is one',
-  `part1` char(2) default NULL COMMENT 'Equivalent 639-1 identifier, if there is one',
+  `part2_b` char(3) DEFAULT NULL COMMENT 'Equivalent 639-2 identifier of the bibliographic applications code set, if there is one',
+  `part2_t` char(3) DEFAULT NULL COMMENT 'Equivalent 639-2 identifier of the terminology applications code set, if there is one',
+  `part1` char(2) DEFAULT NULL COMMENT 'Equivalent 639-1 identifier, if there is one',
   `scope` ENUM('I', 'M', 'S') NOT NULL COMMENT 'I(ndividual), M(acrolanguage), S(pecial)',
   `type` ENUM('A', 'C', 'E', 'H', 'L', 'S') NOT NULL COMMENT 'A(ncient), C(onstructed), E(xtinct), H(istorical), L(iving), S(pecial)',
   `ref_name` varchar(150) NOT NULL COMMENT 'Reference language name',
-  `comment` varchar(150) default NULL COMMENT 'Comment relating to one or more of the columns',
-  `active` TINYINT UNSIGNED NOT NULL default 0 COMMENT 'Is the language visible? (1=yes, 0=no).' ,
+  `comment` varchar(150) DEFAULT NULL COMMENT 'Comment relating to one or more of the columns',
+  `active` TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Is the language visible? (1=yes, 0=no).' ,
   PRIMARY KEY (`id`)
 )
 ENGINE=InnoDB;
@@ -495,7 +495,7 @@ CREATE TABLE IF NOT EXISTS `dnb_institutes` (
     `city` VARCHAR(255) NOT NULL ,
     `phone` VARCHAR(255) ,
     `dnb_contact_id` VARCHAR(255) COMMENT 'Contact id of the german national library.' ,
-    `is_grantor` TINYINT (1) NOT NULL COMMENT 'Flag: is the institituion grantor of academic degrees?' ,
+    `is_grantor` TINYINT (1) NOT NULL DEFAULT 0 COMMENT 'Flag: is the institituion grantor of academic degrees?' ,
     PRIMARY KEY (`id`)
     )
 ENGINE = InnoDB
