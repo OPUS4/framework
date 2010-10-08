061464897205ffe5be125ffad1f183c7f081f941
Jens Schwidder <schwidder@zib.de>
2010-10-08 16:31:06 +0000

    Abstract base class for Opus job workers.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@6475 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ library/Opus/Job/Worker/Abstract.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Framework
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2010, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * Abstract base class for Opus job worker classes.
+ */
+abstract class Opus_Job_Worker_Abstract implements Opus_Job_Worker_Interface {
+
+    /**
+     * Hold current logger instance.
+     *
+     * @var Zend_Log
+     */
+    private $_logger = null;
+
+    /**
+     * Set logging facility.
+     *
+     * @param Zend_Log $logger Logger instance.
+     * @return void
+     */
+    public function setLogger($logger) {
+        if (null === $logger) {
+            $this->_logger = new Zend_Log(new Zend_Log_Writer_Null());
+        } else if ($logger instanceof Zend_Log) {
+            $this->_logger = $logger;
+        } else {
+            throw new IllegalArgumentException('Zend_Log instance expected.');
+        }
+    }
+
+}
+
+?>
