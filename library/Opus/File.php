22fc801957449be061f9d0a21ceb1c2a8752fc57
Thoralf Klein <thoralf@fischlustig.de>
2010-10-26 17:27:20 +0000

    Opus_File: Re-added fields AccessPermission.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@6731 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- library/Opus/File.php
+++ library/Opus/File.php
@@ -70,6 +70,13 @@ class Opus_File extends Opus_Model_Dependent_Abstract {
      * @see Opus_Model_Abstract::$_externalFields
      */
     protected $_externalFields = array(
+        'AccessPermission' => array(
+            'model' => 'Opus_Role',
+            'through' => 'Opus_Model_Dependent_Link_FileRole',
+            'options' => array('privilege' => 'readFile'),
+            'fetch' => 'lazy'
+        ),
+        'TempFile' => array(),
         'HashValue' => array(
             'model' => 'Opus_HashValues'
         ),
@@ -112,6 +119,11 @@ class Opus_File extends Opus_Model_Dependent_Abstract {
         $hashvalue->setMandatory(true)
                 ->setMultiplicity('*');
 
+        $role = new Opus_Model_Field('AccessPermission');
+        $role->setMultiplicity('*');
+        $role->setDefault(Opus_Role::getAll());
+        $role->setSelection(true);
+
         $this->addField($filepathname)
                 ->addField($filelabel)
                 ->addField($mimetype)
@@ -120,7 +132,8 @@ class Opus_File extends Opus_Model_Dependent_Abstract {
                 ->addField($filesize)
                 ->addField($visible_in_frontdoor)
                 ->addField($visible_in_oai)
-                ->addField($hashvalue);
+                ->addField($hashvalue)
+                ->addField($role);
 
         $config = Zend_Registry::get('Zend_Config');
         $workspaceFiles = $config->workspacePath . "/files";
@@ -336,6 +349,16 @@ class Opus_File extends Opus_Model_Dependent_Abstract {
         }
         $this->setHashValue($hashs);
 
+    }
+
+    /**
+     * Overwrite describe: Do not export external fields to XML.
+     *
+     * @return array
+     */
+    public function describe() {
+        $excludeFields = array( 'AccessPermission' );
+        return array_diff(parent::describe(), $excludeFields);
     }
 
 }
