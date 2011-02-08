ae36e28dcff5df62f8f6d017609bffbfe81edd0e
Thoralf Klein <thoralf@fischlustig.de>
2011-02-08 16:39:02 +0000

    framework/Opus_Mail_SendMail: Refactoring: Removed even more boiler-plate code. framework/Opus_Mail_SendMail: Fixed copyright note and comment.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@7502 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- library/Opus/Mail/SendMail.php
+++ library/Opus/Mail/SendMail.php
@@ -26,14 +26,15 @@
  *
  * @category    Framework
  * @package     Opus_Mail
+ * @author      Thoralf Klein <thoralf.klein@zib.de>
  * @author      Eva Kranz <s9evkran@stud.uni-saarland.de>
- * @copyright   Copyright (c) 2009, OPUS 4 development team
+ * @copyright   Copyright (c) 2011, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  * @version     $Id$
  */
 
 /**
- * Methods to send an e-mail or multiple e-mails via Zend_Mail from different views.
+ * Methods to send e-mails via Zend_Mail, but with mail server from config.ini.
  *
  * @category    Framework
  * @package     Opus_Mail
@@ -45,41 +46,6 @@ class Opus_Mail_SendMail {
      * @var Opus_Mail_Transport
      */
     private $_transport = null;
-
-    /**
-     * Holds the e-mail address of the sender
-     *
-     * @var string
-     */
-    private $_from;
-
-    /**
-     * Holds the name of the sender
-     *
-     * @var string
-     */
-    private $_fromName;
-
-    /**
-     * Holds the e-mail addresses and names of the recipients
-     *
-     * @var array
-     */
-    private $_recipients;
-
-    /**
-     * Holds the subject of the e-mail
-     *
-     * @var string
-     */
-    private $_subject;
-
-    /**
-     * Holds the text of the e-mail
-     *
-     * @var string
-     */
-    private $_bodyText;
 
     /**
      * Create a new SendMail instance
@@ -94,111 +60,13 @@ class Opus_Mail_SendMail {
     }
 
     /**
-     * Set the recipients
-     *
-     * @param   array $recipients Recipients
-     * @return  void
-     */
-    public function setRecipients(array $recipients) {
-        $this->_recipients = $recipients;
-    }
-
-    /**
-     * Get the recipients
-     *
-     * @return array recipients
-     */
-    public function getRecipients() {
-        return $this->_recipients;
-    }
-
-    /**
-     * Set the e-mail address of the sender
-     *
-     * @param   string $from Sender's e-mail address
-     * @return  void
-     */
-    public function setFrom($from) {
-        $this->validateAddress($from);
-        $this->_from = $from;
-    }
-
-    /**
-     * Get the e-mail address of the sender
-     *
-     * @return string Sender's e-mail address
-     */
-    public function getFrom() {
-        return $this->_from;
-    }
-
-    /**
-     * Set the name of the sender
-     *
-     * @param   string $fromName Sender's name
-     * @return  void
-     */
-    public function setFromName($fromName) {
-        $this->_fromName = $fromName;
-    }
-
-    /**
-     * Get the name of the sender
-     *
-     * @return string Sender's name
-     */
-    public function getFromName() {
-        return $this->_fromName;
-    }
-
-    /**
-     * Set the subject of the e-mail
-     *
-     * @param   string $subject Subject
-     * @return  void
-     */
-    public function setSubject($subject) {
-        $this->_subject = $subject;
-    }
-
-    /**
-     * Get the subject of the e-mail
-     *
-     * @return string Subject
-     */
-    public function getSubject() {
-        return $this->_subject;
-    }
-
-    /**
-     * Set the text of the e-mail
-     *
-     * @param   string $bodyText Text
-     * @return  void
-     */
-    public function setBodyText($bodyText) {
-        $this->_bodyText = $bodyText;
-    }
-
-    /**
-     * Get the text of the e-mail
-     *
-     * @return string Text
-     */
-    public function getBodyText() {
-        return $this->_bodyText;
-    }
-
-    /**
      * Validates an e-mail address
      *
      * @param   string $address Address
      * @throws  Opus_Mail_Exception Thrown if the e-mail address is not valid
      * @return  string              Address
      */
-    private function validateAddress($address) {
-        return $address;
-
+    public static function validateAddress($address) {
         $validator = new Zend_Validate_EmailAddress();
         if ($validator->isValid($address) === false) {
             foreach ($validator->getMessages() as $message) {
@@ -219,25 +87,12 @@ class Opus_Mail_SendMail {
      * @param   string $subject    Subject
      * @param   string $bodyText   Text
      * @param   array  $recipients Recipients (array [#] => array ('name' => '...', 'address' => '...'))
+     *
      * @return  boolean            True if mail was sent
+     * @throws Opus_Mail_Exception Thrown if the mail could not be sent.
+     * @throws Opus_Mail_Exception Thrown if the from address is invalid.
      */
     public function sendMail($from, $fromName, $subject, $bodyText, array $recipients) {
-        $this->setRecipients($recipients);
-        $this->setSubject($subject);
-        $this->setBodyText($bodyText);
-        $this->setFrom($from);
-        $this->setFromName($fromName);
-        return $this->send();
-    }
-
-    /**
-     * Composes an e-mail for multiple recipients from the specified components.
-     *
-     * @throws Opus_Mail_Exception Thrown if the number of recipient names and of recipient addresses differ
-     * @throws Opus_Mail_Exception Thrown if the mail could not be sent
-     * @return boolean             True if mail could be sent
-     */
-    private function send() {
         $logger = Zend_Registry::get('Zend_Log');
 
         if (!isset($this->_transport)) {
@@ -245,15 +100,10 @@ class Opus_Mail_SendMail {
             return true;
         }
 
-        $recipients = $this->getRecipients();
-        $from = $this->getFrom();
-        $fromName = $this->getFromName();
-        $subject = $this->getSubject();
-        $text = $this->getBodyText();
-
         if (trim($from) === '') {
             throw new Opus_Mail_Exception('No sender address given.');
         }
+        self::validateAddress($from);
 
         if (trim($subject) === '') {
             throw new Opus_Mail_Exception('No subject text given.');
@@ -262,7 +112,7 @@ class Opus_Mail_SendMail {
         $mail = new Zend_Mail('utf-8');
         $mail->setFrom($from, $fromName);
         $mail->setSubject($subject);
-        $mail->setBodyText($text);
+        $mail->setBodyText($bodyText);
 
         foreach ($recipients as $recip) {
             $mail->addTo($recip['address'], $recip['name']);
@@ -273,7 +123,7 @@ class Opus_Mail_SendMail {
             $logger->debug('SendMail: Successfully sent mail to ' . $recip['address']);
         } catch (Exception $e) {
             $logger->err('SendMail: Failed sending mail to ' . $recip['address'] . ', error: ' . $e);
-            throw new Opus_Mail_Exception('One or more mails could not be sent.');
+            throw new Opus_Mail_Exception('SendMail: Mails could not be sent.');
         }
 
         return true;
