4b52960c7d2598521aaf99cf5ae34a7fb394e7ff
Thoralf Klein <thoralf@fischlustig.de>
2011-02-25 17:50:19 +0000

    framework/Opus_Collection: Added field "ParentId" for getting the collection id of the parent node.  (Fixes #OPUSVIER-1092.)
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@7670 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- library/Opus/Collection.php
+++ library/Opus/Collection.php
@@ -114,7 +114,7 @@ class Opus_Collection extends Opus_Model_AbstractDb {
      */
     protected function _init() {
 
-        $fields = array('Number', 'Name', 'OaiSubset', 'SortOrder',
+        $fields = array('Number', 'Name', 'OaiSubset', 'SortOrder', 'ParentId',
             'RoleId', 'Role', 'RoleName',
             'RoleDisplayFrontdoor', 'RoleVisibleFrontdoor');
         foreach ($fields as $field) {
