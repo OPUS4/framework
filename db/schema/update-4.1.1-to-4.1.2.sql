e011179aceb303765d051a2996bd2a5a35a66187
Thoralf Klein <thoralf@fischlustig.de>
2011-07-22 17:18:10 +0000

    BUGFIX: Changed YEAR fields to UNSIGNED SMALLINT, because YEAR only allows values from 1901 to 2155.  (Fixes OPUSVIER-1511.)
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@8691 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/schema/update-4.1.1-to-4.1.2.sql
+++ db/schema/update-4.1.1-to-4.1.2.sql
@@ -1,3 +1,18 @@
 ALTER TABLE `document_xml_cache`
   ENGINE = InnoDB
   COMMENT = 'Caches XML for Opus_Document objects.';
+
+
+-- Change YEAR fields to UNSIGNED SMALLINT (see OPUSVIER-1511)
+-- MYSQL YEAR only allows values from 1901 to 2155
+ALTER TABLE  `documents` CHANGE  `completed_year`  `completed_year` YEAR( 4 ) NULL DEFAULT NULL;
+UPDATE `documents` SET completed_year = NULL WHERE completed_year = '0000';
+ALTER TABLE  `documents` CHANGE  `completed_year`  `completed_year` SMALLINT( 4 ) UNSIGNED ZEROFILL NULL DEFAULT NULL;
+
+ALTER TABLE  `documents` CHANGE  `published_year`  `published_year` YEAR( 4 ) NULL DEFAULT NULL;
+UPDATE `documents` SET published_year = NULL WHERE published_year = '0000';
+ALTER TABLE  `documents` CHANGE  `published_year`  `published_year` SMALLINT( 4 ) UNSIGNED ZEROFILL NULL DEFAULT NULL;
+
+ALTER TABLE  `document_patents` CHANGE  `year_applied`  `year_applied` YEAR( 4 ) NULL DEFAULT NULL;
+UPDATE `document_patents` SET year_applied = NULL WHERE year_applied = '0000';
+ALTER TABLE  `document_patents` CHANGE  `year_applied`  `year_applied` SMALLINT( 4 ) UNSIGNED ZEROFILL NOT NULL;
