95601813a628822b27f71ab60243b3501a09887d
Thoralf Klein <thoralf@fischlustig.de>
2011-12-12 15:16:03 +0000

    framework: Trivial error handling in Opus_Model_Xml_Version1: Only try getId on objects of AbstractDb.
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@9368 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- library/Opus/Model/Xml/Version1.php
+++ library/Opus/Model/Xml/Version1.php
@@ -258,7 +258,11 @@ class Opus_Model_Xml_Version1 implements Opus_Model_Xml_Strategy {
         }
 
         $childNode = $dom->createElement(get_class($model));
-        $childNode->setAttribute('Id', $model->getId());
+
+        if ($model instanceof Opus_Model_AbstractDb) {
+            $childNode->setAttribute('Id', $model->getId());
+        }
+
         $rootNode->appendChild($childNode);
 
         foreach ($fields_diff as $fieldname) {
