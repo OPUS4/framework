38282f741187c3bf59f4525b34d211e883088021
Sascha Szott <szott@gmx.de>
2011-12-13 12:41:50 +0000

    review-process: fixed typos; added two unit tests Issue #OPUSVIER-1851 - Schemaänderung Enrichments: globale Registry für die Enrichments anlegen (Modellierung einer n-zu-m statt einer 1-zu-n Relation zwischen Dokumenten und Enrichments)
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@9373 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/Opus/EnrichmentTest.php
+++ tests/Opus/EnrichmentTest.php
@@ -60,16 +60,20 @@ class Opus_EnrichmentTest extends TestCase {
         $this->_enrichmentkey = new Opus_EnrichmentKey();
         $this->_enrichmentkey->setName('valid');
         $this->_enrichmentkey->store();
+
+        $this->_anotherenrichmentkey = new Opus_EnrichmentKey();
+        $this->_anotherenrichmentkey->setName('anothervalid');
+        $this->_anotherenrichmentkey->store();
     }
+
     /*
     public function tearDown() {
         $this->_doc->deletePermanent();
         $this->_enrichmentkey->delete();
-
+        $this->_anotherenrichmentkey->delete();
         parent::tearDown();
     }
-     * 
-     */
+    */
 
     /**
      * Stores and loads a document with an enrichment and asserts
@@ -97,7 +101,7 @@ class Opus_EnrichmentTest extends TestCase {
                 'Loaded other value, then stored.');
     }
 
-    public function testStoreAndLoadEqualKeyEnrichemts() {
+    public function testStoreAndLoadEqualKeyEnrichments() {
         $key = 'valid';
         $values = array('foo', 'bar');
 
@@ -126,7 +130,33 @@ class Opus_EnrichmentTest extends TestCase {
         }
     }
 
-    public function testStoreAndLoadEqualEnrichemts() {
+    public function testStoreAndLoadSimpleEnrichments() {
+        $keys = array('valid', 'anothervalid');
+        $value = 'foo';
+
+        foreach ($keys as $key) {
+            $this->_doc->addEnrichment()->setKeyName($key)->setValue($value);
+        }
+        $this->_doc->store();
+
+        // reload document
+        $doc = new Opus_Document($this->_doc->getId());        
+        $enrichments = $doc->getEnrichment();
+        $this->assertEquals(count($keys), count($enrichments),
+                'Stored one enrichment, loaded more or less enrichments.');
+
+        $enrichmentKeys = array();
+        foreach ($enrichments as $enrichment) {
+            $this->assertEquals($value, $enrichment->getValue(), 'Loaded other value, then stored.');
+            array_push($enrichmentKeys, $enrichment->getKeyName());
+        }
+        
+        foreach ($keys as $key) {
+            $this->assertTrue(in_array($key, $enrichmentKeys), "enrichment key $key does not exists");
+        }
+    }
+
+    public function testStoreAndLoadEqualEnrichments() {
         $key = 'valid';
         $value = 'foo';
         $count = 5;
@@ -192,6 +222,32 @@ class Opus_EnrichmentTest extends TestCase {
 
         $doc = new Opus_Document( $this->_doc->getId() );
         $this->assertEquals(0, count($doc->getEnrichment()));
+    }
+
+    public function testStoreIdenticalEnrichmentKeys() {
+        $key = 'valid';
+        $value = 'foo';
+
+        $this->_doc->addEnrichment()->setKeyName($key)->setValue($value);
+        $this->_doc->store();
+
+        $this->_doc->addEnrichment()->setKeyName($key)->setValue($value);
+        $this->_doc->store();
+
+        //reload document
+        $doc = new Opus_Document($this->_doc->getId());
+        //get array of enrichments
+        $enrichments = $doc->getEnrichment();
+        $this->assertEquals(2, count($enrichments),
+                'Count of stored and loaded enrichments differ!');
+
+        // compare enrichment values
+        foreach ($enrichments as $e) {
+            $this->assertEquals($key, $e->getKeyName(),
+                    'Loaded other key, then stored.');
+            $this->assertEquals($value, $e->getValue(),
+                    'Loaded other value, then stored.');
+        }
     }
 
 }
