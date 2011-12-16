a248fe5705a8eb49b06ae4ca87f406bc5d1b04cd
Susanne Gottwald <gottwald@zib.de>
2011-12-16 12:08:32 +0000

    BUGFIX: changed table columns: logo, publisher are optional, number now has type varchar Issue #OPUSVIER-2030 - Anpassungen am Datenbank-Design: NOT NULL Constraints entfernen; Anpassung Datentyp des Felds number
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@9445 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/schema/opus420.sql
+++ db/schema/opus420.sql
@@ -145,8 +145,8 @@ COMMENT = 'Table for subject heading related data.';
 CREATE TABLE IF NOT EXISTS `document_sets` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` mediumtext NOT NULL COMMENT 'Title of document set (e.g. series)',
-  `logo` text NOT NULL COMMENT 'Pfad zum Logo des Containers',
-  `publisher` mediumtext NOT NULL COMMENT 'Name of Publisher',
+  `logo` text COMMENT 'Pfad zum Logo des Containers',
+  `publisher` mediumtext COMMENT 'Name of Publisher',
   `issn` mediumtext COMMENT 'ISSN of that document set',
   `infobox` text COMMENT 'html-fähige Infobox',
   PRIMARY KEY (`id`)
@@ -159,7 +159,7 @@ CREATE TABLE IF NOT EXISTS `document_sets` (
 CREATE TABLE IF NOT EXISTS `link_documents_sets` (
   `document_id` int(10) unsigned NOT NULL,
   `set_id` int(10) unsigned NOT NULL,
-  `number` int(11) NOT NULL COMMENT 'corresponding number (e.g. serial number)',
+  `number` varchar(20) NOT NULL COMMENT 'corresponding number (e.g. serial number)',
   PRIMARY KEY (`document_id`,`set_id`),
   UNIQUE KEY `document_id` (`document_id`,`number`),
   KEY `set_id` (`set_id`),
