720d2fcdf7b90bbfd27d1bd301ade0609b392121
Thoralf Klein <thoralf@fischlustig.de>
2012-01-05 20:23:47 +0000

    framework: Re-added default values for Opus_Subject fields "Types".  Has unseen dependencies to Controller_Helper_TranslationTest::testTranslationOfSubjectTypeValues().  (See OPUSVIER-1941)
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@9662 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- library/Opus/Subject.php
+++ library/Opus/Subject.php
@@ -75,6 +75,13 @@ class Opus_Subject extends Opus_Model_Dependent_Abstract
         $type = new Opus_Model_Field('Type');
         $type->setMandatory(true);
         $type->setSelection(true);
+        $type->setDefault(array(
+            'swd' => 'swd',
+            'psyndex' => 'psyndex',
+            'uncontrolled' => 'uncontrolled',
+            'msc' => 'msc',
+            'ddc' => 'ddc'
+        ));
         
         $value = new Opus_Model_Field('Value');
         $value->setMandatory(true)
