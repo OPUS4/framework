b55528cb770787063b23b5d4ee698e85f4d24557
Thoralf Klein <thoralf@fischlustig.de>
2012-06-12 10:18:25 +0000

    database update scripts: outstanding DB update for collection-role "open_access".  (Fixes #OPUSVIER-1917)
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@10328 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/schema/update-4.2.0-to-4.2.1.sql
+++ db/schema/update-4.2.0-to-4.2.1.sql
@@ -1,3 +1,10 @@
---
--- this file was left intentionally empty since no database changes need to be made
---
+INSERT INTO `collections_roles` (`name`, `oai_name`, `position`, `visible`, `visible_browsing_start`, `display_browsing`, `visible_frontdoor`, `display_frontdoor`, `visible_oai`) VALUES
+('open_access', 'open_access', 100, 1, 0, 'Name', 0, 'Name', 1);
+SET @roleid = LAST_INSERT_ID();
+
+INSERT INTO `collections` (`role_id`, `number`, `name`, `oai_subset`, `sort_order`, `left_id`, `right_id`, `parent_id`, `visible`) VALUES
+(@roleid, NULL, NULL, NULL, 0, 1, 4, NULL, 1);
+SET @rootid = LAST_INSERT_ID();
+
+INSERT INTO `collections` (`role_id`, `number`, `name`, `oai_subset`, `sort_order`, `left_id`, `right_id`, `parent_id`, `visible`) VALUES
+(@roleid, '', 'open_access', 'open_access', 0, 2, 3, @rootid, 1);
