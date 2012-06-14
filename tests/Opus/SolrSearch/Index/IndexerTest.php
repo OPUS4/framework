e5ede4c07d504afe4b64bc9cab888ce2c747ba80
Sascha Szott <szott@gmx.de>
2012-06-14 15:51:15 +0000

    added test case 'testFulltextVisibilityIsConsideredInFacet()' as suggested by reviewer Issue #OPUSVIER-2417 - Sichtbarkeit und Lesezugriffsrecht von Dateien wird bei der Befüllung des Indexfeld has_fulltext ignoriert
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@10413 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/Opus/SolrSearch/Index/IndexerTest.php
+++ tests/Opus/SolrSearch/Index/IndexerTest.php
@@ -642,5 +642,26 @@ class Opus_SolrSearch_Index_IndexerTest extends TestCase {
         $this->assertNotContains('<field name="has_fulltext">true</field>', $xml->saveXML());        
     }
 
+    /**
+     * Regression test for OPUSVIER-2417
+     */
+    public function testFulltextVisibilityIsConsideredInFacet() {
+        $doc = new Opus_Document();
+        $doc->setServerState('published');
+        $doc->setLanguage('eng');
+        $file = $doc->addFile();
+        $file->setPathName('nonexistent.pdf');
+        $file->setVisibleInFrontdoor('1');
+        $doc->store();
+
+        $class = new ReflectionClass('Opus_SolrSearch_Index_Indexer');
+        $method = $class->getMethod('getSolrXmlDocument');
+        $method->setAccessible(true);
+
+        $xml = $method->invoke ($this->indexer, $doc);
+        $this->assertContains('<field name="has_fulltext">true</field>', $xml->saveXML());
+        $this->assertNotContains('<field name="has_fulltext">false</field>', $xml->saveXML());
+    }
+
 }
 
