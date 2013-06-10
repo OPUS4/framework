081c970788c34d8e9470ee4f51f19cd98a626bb9
Jens Schwidder <schwidder@zib.de>
2013-06-10 11:26:14 +0000

    SQL Schema so geändert, daß NULL für Patent Jahr erlaubt wird. Ein Statement für die Modifikation der Tabelle beim Update hinzugefügt. Issue #OPUSVIER-2824 - beim Anlegen eines Patents unter ausschließlicher Angabe einer Nummer wird automatisch für das YearApplied der Wert 0000 gespeichert
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@11374 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/schema/update-4.3.1-to-4.4.0.sql
+++ db/schema/update-4.3.1-to-4.4.0.sql
@@ -1,3 +1,8 @@
 --
 -- this file was left intentionally empty since no database changes need to be made
 --
+
+-- Erlaube NULL fuer YearApplied Feld in Opus_Patent
+ALTER TABLE `documents`
+    MODIFY COLUMN `year_applied` SMALLINT( 4 ) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT 'Year of the application.';
+
