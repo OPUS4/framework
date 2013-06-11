f66712f99b0518feb76d1d9c9431c637fad6749b
Gunar Maiwald <maiwald@zib.de>
2013-06-11 10:23:36 +0000

    - Model für den BibtexImport - Stylesheet für die Transformation von Mods nach OpusXml - Neuer Enrichmentkex (BibtexRecord) in schema
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@11396 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/schema/update-4.3.1-to-4.4.0.sql
+++ db/schema/update-4.3.1-to-4.4.0.sql
@@ -6,3 +6,11 @@
 ALTER TABLE `documents`
     MODIFY COLUMN `year_applied` SMALLINT( 4 ) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT 'Year of the application.';
 
+-- if standardized enrichment_key_names from bibtex-upload are already used set them to a temporary name
+
+UPDATE `document_enrichments` SET `key_name` = 'TempBibtexRecord' WHERE `key_name` = 'BibtexRecord';
+UPDATE `enrichmentkeys`SET `name`= 'TempBibtexRecord' WHERE `name` = 'BibtexRecord';
+
+INSERT INTO `enrichmentkeys` (`name`) VALUES
+  ('BibtexRecord');
+
