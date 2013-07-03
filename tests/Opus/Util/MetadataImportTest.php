2108b6669527dc732a52f45696a4bba4d8e12397
Sascha Szott <szott@gmx.de>
2013-07-03 12:33:32 +0000

    minor change: fixed broken prolog in framework test class files
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@11931 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/Opus/Util/MetadataImportTest.php
+++ tests/Opus/Util/MetadataImportTest.php
@@ -20,8 +20,7 @@
  * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details. You should        }
- have received a copy of the GNU General Public License
+ * details. You should have received a copy of the GNU General Public License
  * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
@@ -32,7 +31,6 @@
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  * @version     $Id$
  */
-
 
 class Opus_Util_MetadataImportTest extends PHPUnit_Framework_TestCase {
 
