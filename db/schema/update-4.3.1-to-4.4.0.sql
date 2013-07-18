2f786da5c4a2615ba1ed3c083698160c0a45b999
Sascha Szott <szott@gmx.de>
2013-07-18 09:47:41 +0000

    added transaction context for database changes that need to be executed when updating to OPUS 4.4 Issue #OPUSVIER-2724 - leeres DB-Update-Skript db/schema/update-4.3.1-to-4.4.0.sql anlegen
    git-svn-id: https://svn.zib.de/opus4dev/framework/trunk@12175 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/schema/update-4.3.1-to-4.4.0.sql
+++ db/schema/update-4.3.1-to-4.4.0.sql
@@ -1,16 +1,21 @@
---
--- this file was left intentionally empty since no database changes need to be made
---
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES';
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, AUTOCOMMIT=0;
 
--- Erlaube NULL fuer YearApplied Feld in Opus_Patent
-ALTER TABLE `documents`
-    MODIFY COLUMN `year_applied` SMALLINT( 4 ) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT 'Year of the application.';
+START TRANSACTION;
+
+-- erlaube NULL fuer YearApplied Feld in Opus_Patent
+ALTER TABLE `documents` MODIFY COLUMN `year_applied` SMALLINT( 4 ) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT 'Year of the application.';
 
 -- if standardized enrichment_key_names from bibtex-upload are already used set them to a temporary name
-
 UPDATE `document_enrichments` SET `key_name` = 'TempBibtexRecord' WHERE `key_name` = 'BibtexRecord';
 UPDATE `enrichmentkeys`SET `name`= 'TempBibtexRecord' WHERE `name` = 'BibtexRecord';
+INSERT INTO `enrichmentkeys` (`name`) VALUES ('BibtexRecord');
 
-INSERT INTO `enrichmentkeys` (`name`) VALUES
-  ('BibtexRecord');
+COMMIT;
 
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
