f8ad81bf261824ef01cf6ccd5fba3bc76f79f905
j3nsch <schwidder@zib.de>
2017-05-17 16:21:54 +0200

    OPUSVIER-3764 - Added regression test.

--- tests/Opus/PersonTest.php
+++ tests/Opus/PersonTest.php
@@ -554,4 +554,34 @@ class Opus_PersonTest extends TestCase {
         $this->assertEquals(10, $count);
     }
 
+    /**
+     * OPUSVIER-3764
+     *
+     * @expectedException Opus_Model_Exception
+     * @expectedExceptionMessage No Opus_Db_Documents with id
+     */
+    public function testDeleteAssignedPerson()
+    {
+        $this->markTestIncomplete('TODO not sure what/how to test');
+
+        $doc = new Opus_Document();
+        $doc->setServerState('published');
+        $doc->setType('article');
+
+        $person = new Opus_Person();
+        $person->setLastName('Tester');
+
+        $doc->addPersonAuthor($person);
+
+        $docId = $doc->store();
+
+        $person->delete();
+
+        // $doc = new Opus_Document($docId);
+
+        $doc->deletePermanent();
+
+        new Opus_Document($docId);
+    }
+
 }
