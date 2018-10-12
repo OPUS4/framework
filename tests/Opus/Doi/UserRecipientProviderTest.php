c75cc5234b9120bb45386570a159ef480d65213f
j3nsch <j3nsch@gmail.com>
2018-10-12 18:09:10 +0200

    OPUSVIER-3919 Recipient provider using accounts and permissions.

---
+++ tests/Opus/Doi/UserRecipientProviderTest.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Tests
+ * @package     Opus_Doi
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2018, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ */
+
+class Opus_Doi_UserRecipientProviderTest extends TestCase
+{
+
+    public function testGetRecipients()
+    {
+        $role = new Opus_UserRole();
+        $role->setName('DOI');
+        $role->appendAccessModule('doi_notification');
+        $role->store();
+
+        $account = new Opus_Account();
+        $account->addRole($role);
+        $account->setFirstName('John');
+        $account->setLastName('Doe');
+        $account->setEmail('john@localhost');
+        $account->setLogin('john');
+        $account->setPassword('123456');
+        $account->store();
+
+        // Account without name
+        $account = new Opus_Account();
+        $account->addRole($role);
+        $account->setEmail('jane@localhost');
+        $account->setLogin('jane');
+        $account->setPassword('123456');
+        $account->store();
+
+        // Account without permission
+        $account = new Opus_Account();
+        $account->setLogin('tom');
+        $account->setPassword('123456');
+        $account->store();
+
+        // Account without email will not be included
+        $account = new Opus_Account();
+        $account->addRole($role);
+        $account->setFirstName('Paul');
+        $account->setLastName('Miller');
+        $account->setLogin('paul');
+        $account->setPassword('123456');
+        $account->store();
+
+        $provider = new Opus_Doi_UserRecipientProvider();
+
+        $recipients = $provider->getRecipients();
+
+        $this->assertCount(2, $recipients);
+
+        $this->assertEquals([
+            ['name' => 'John Doe', 'address' => 'john@localhost'],
+            ['name' => 'jane', 'address' => 'jane@localhost'],
+        ], $recipients);
+    }
+
+    public function testGetRecipientsFilterAccountsWithoutEmail()
+    {
+        $role = new Opus_UserRole();
+        $role->setName('DOI');
+        $role->appendAccessModule('doi_notification');
+        $role->store();
+
+        $account = new Opus_Account();
+        $account->addRole($role);
+        $account->setFirstName('Paul');
+        $account->setLastName('Miller');
+        $account->setLogin('paul');
+        $account->setPassword('123456');
+        $account->store();
+
+        $provider = new Opus_Doi_UserRecipientProvider();
+
+        $recipients = $provider->getRecipients();
+
+        $this->assertInternalType('array', $recipients);
+        $this->assertCount(0, $recipients);
+    }
+
+    public function testGetRecipientsNone() {
+        $provider = new Opus_Doi_UserRecipientProvider();
+
+        $recipients = $provider->getRecipients();
+
+        $this->assertInternalType('array', $recipients);
+        $this->assertCount(0, $recipients);
+    }
+}
