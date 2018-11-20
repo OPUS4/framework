c2214bfca0cfe5b95946ca5c9defd4c029cb8961
j3nsch <j3nsch@gmail.com>
2018-11-20 16:24:29 +0100

    OPUSVIER-3930 Add class and script for utf8mb4 conversion.

---
+++ db/updatedb-utf8mb4.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Framework
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2018, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ */
+
+/**
+ * Script for converting database to 'utf8mb4'.
+ */
+
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(dirname(__FILE__))));
+
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+// Configure include path.
+set_include_path(
+    implode(
+        PATH_SEPARATOR, array(
+            '.',
+            dirname(__FILE__),
+            APPLICATION_PATH . '/library',
+            APPLICATION_PATH . '/vendor',
+            get_include_path(),
+        )
+    )
+);
+
+require_once 'autoload.php';
+
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    array(
+        "config"=>array(
+            APPLICATION_PATH . '/tests/config.ini',
+            APPLICATION_PATH . '/tests/tests.ini'
+        )
+    )
+);
+
+Zend_Registry::set('opus.disableDatabaseVersionCheck', true);
+
+// Bootstrapping application
+$application->bootstrap('Backend');
+
+$update = new Opus_Update_Plugin_DatabaseCharset();
+
+$update->run();
+
+$color = "\033[1;33m";
+
+echo PHP_EOL;
+echo  "{$color}After converting the database to 'utf8mb4' you should run 'repair' and 'optimize' on your database."
+    . ' The following command performs these operations for all databases. See your database documentation for more'
+    . ' information.';
+echo PHP_EOL . PHP_EOL . "{$color}$ mysqlcheck -u root -p --auto-repair --optimize --all-databases";
+echo PHP_EOL . PHP_EOL;
