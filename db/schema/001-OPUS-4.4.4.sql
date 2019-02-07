a42fe5f68fe739768ea59c95264f3fd849014a7c
j3nsch <j3nsch@gmail.com>
2019-02-07 14:47:18 +0100

    OPUSVIER-3985 Reduced column length back to 191 chars.

--- db/schema/001-OPUS-4.4.4.sql
+++ db/schema/001-OPUS-4.4.4.sql
@@ -562,8 +562,8 @@ CREATE  TABLE IF NOT EXISTS `languages` (
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `dnb_institutes` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(192) NOT NULL,
-  `department` varchar(192) DEFAULT NULL,
+  `name` VARCHAR(191) NOT NULL,
+  `department` varchar(191) DEFAULT NULL,
   `address` MEDIUMTEXT ,
   `city` VARCHAR(255) NOT NULL ,
   `phone` VARCHAR(255) ,
