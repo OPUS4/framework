15734e434f9fa9da9481f4d373e33beb0f97ff6c
j3nsch <j3nsch@gmail.com>
2019-02-08 17:22:17 +0100

    OPUSVIER-3433 Coding style.

--- library/Opus/Version.php
+++ library/Opus/Version.php
@@ -28,7 +28,7 @@
  * @package     Opus
  * @author      Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
  * @author      Jens Schwidder <schwidder@zib.de>
- * @copyright   Copyright (c) 2008-2016, OPUS 4 development team
+ * @copyright   Copyright (c) 2008-2019, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  */
 
