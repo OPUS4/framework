207a13ef04c8ddf27dce1d1ae93de2c8f394a85a
j3nsch <j3nsch@gmail.com>
2019-04-16 15:59:56 +0200

    OPUSVIER-968 Using Trait for plugins functions.

--- library/Opus/Document/Plugin/SequenceNumber.php
+++ library/Opus/Document/Plugin/SequenceNumber.php
@@ -38,13 +38,14 @@
  * @package     Opus_Document_Plugin
  * @uses        Opus_Model_Plugin_Abstract
  */
-class Opus_Document_Plugin_SequenceNumber extends Opus_Model_Plugin_Abstract {
+class Opus_Document_Plugin_SequenceNumber extends Opus_Model_Plugin_Abstract
+{
 
     /**
      * @see {Opus_Model_Plugin_Interface::postStore}
      */
-    public function postStoreInternal(Opus_Model_AbstractDb $model) {
-
+    public function postStoreInternal(Opus_Model_AbstractDb $model)
+    {
         $log = Zend_Registry::get('Zend_Log');
         $log->debug('Opus_Document_Plugin_SequenceNumber::postStore() with id ' . $model->getId());
 
@@ -67,34 +68,36 @@ class Opus_Document_Plugin_SequenceNumber extends Opus_Model_Plugin_Abstract {
         }
         $sequence_type = trim($config->sequence->identifier_type);
 
-        $sequence_ids = array();
+        $sequence_ids = [];
+
         foreach ($model->getIdentifier() AS $id) {
             if ($id->getType() === $sequence_type) {
                 $sequence_ids[] = trim($id->getValue());
             }
         }
-        
+
         if (count($sequence_ids) > 0) {
             $message = "Sequence IDs for type '$sequence_type' already exists: "
                 . implode(",", $sequence_ids);
             $log->debug($message);
             return;
         }
-        
+
         // Create and initialize new sequence number...
         $next_sequence_number = $this->_fetchNextSequenceNumber($sequence_type);
-        
+
         $model->addIdentifier()
             ->setType($sequence_type)
             ->setValue($next_sequence_number);
-        
+
         return;
     }
 
     /**
      * Small helper method to fetch next sequence number from database.
      */
-    protected function _fetchNextSequenceNumber($sequence_type) {
+    protected function _fetchNextSequenceNumber($sequence_type)
+    {
         $id_table = Opus_Db_TableGateway::getInstance('Opus_Db_DocumentIdentifiers');
         $select = $id_table->select()->from($id_table, '')
                 ->columns(new Zend_Db_Expr('MAX(CAST(value AS SIGNED))'))
@@ -105,9 +108,7 @@ class Opus_Document_Plugin_SequenceNumber extends Opus_Model_Plugin_Abstract {
         if (is_null($last_sequence_id) or $last_sequence_id <= 0) {
             return 1;
         }
-        
+
         return $last_sequence_id + 1;
     }
-    
 }
-
--- library/Opus/Model/AbstractDb.php
+++ library/Opus/Model/AbstractDb.php
@@ -30,9 +30,11 @@
  * @author      Pascal-Nicolas Becker <becker@zib.de>
  * @author      Thoralf Klein <thoralf.klein@zib.de>
  * @author      Jens Schwidder <schwidder@zib.de>
- * @copyright   Copyright (c) 2008-2018, OPUS 4 development team
+ * @copyright   Copyright (c) 2008-2019, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  */
+
+use Opus\Model\PluginsTrait;
 
 /**
  * Abstract class for all domain models in the Opus framework that are connected
@@ -44,6 +46,8 @@
 
 abstract class Opus_Model_AbstractDb extends Opus_Model_Abstract implements Opus_Model_ModificationTracking
 {
+
+    use Opus\Model\PluginsTrait;
 
     /**
      * TODO: Change name of this array to somewhat more general.
@@ -235,78 +239,6 @@ abstract class Opus_Model_AbstractDb extends Opus_Model_Abstract implements Opus
         return parent::addField($field);
     }
 
-     /**
-     * Instanciate and install plugins for this model.
-     *
-     * Copy-Paste from Qucosa-Code base.
-     *
-     * @return void
-     */
-    protected function _loadPlugins()
-    {
-        foreach ($this->_plugins as $pluginname => $plugin) {
-            if (true === is_string($plugin)) {
-                $pluginname = $plugin;
-                $plugin = null;
-            }
-
-            if (null === $plugin) {
-                $plugin = new $pluginname;
-            }
-
-            $this->registerPlugin($plugin);
-        }
-    }
-
-    /**
-     * Register a pre- or post processing plugin.
-     *
-     * Copy-Paste from Qucosa-Code base.
-     *
-     * @param Opus_Model_Plugin_Interface $plugin Plugin to register for this very model.
-     * @return void
-     */
-    public function registerPlugin(Opus_Model_Plugin_Interface $plugin)
-    {
-        $this->_plugins[get_class($plugin)] = $plugin;
-    }
-
-    /**
-     * Unregister a pre- or post processing plugin.
-     *
-     * Copy-Paste from Qucosa-Code base.
-     *
-     * @param string|object $plugin Instance or class name to unregister plugin.
-     * @throw Opus_Model_Exception Thrown if specified plugin does not exist.
-     * @return void
-     */
-    public function unregisterPlugin($plugin)
-    {
-        $key = '';
-        if (true === is_string($plugin)) {
-            $key = $plugin;
-        }
-        if (true === is_object($plugin)) {
-            $key = get_class($plugin);
-        }
-        if (false === isset($this->_plugins[$key])) {
-            // don't throw exception, just write a warning
-            $this->getLogger()->warn('Cannot unregister specified plugin: ' . $key);
-        }
-        else {
-            unset($this->_plugins[$key]);
-        }
-    }
-
-    /**
-     * Return true if the given plugin was already registered; otherwise false.
-     * @param string $plugin class name of the plugin
-     */
-    public function hasPlugin($plugin)
-    {
-        return array_key_exists($plugin, $this->_plugins);
-    }
-
     /**
      * Fetch attribute values from the table row and set up all fields. If fields containing
      * dependent models or link models those got fetched too.
@@ -363,34 +295,6 @@ abstract class Opus_Model_AbstractDb extends Opus_Model_Abstract implements Opus
             }
             // Clear the modified flag for the just loaded field
             $field->clearModified();
-        }
-    }
-
-    /**
-     * Calls a specified plugin method in all available plugins.
-     *
-     * Copy-Paste from Qucosa-Code base.
-     *
-     * @param string $methodname Name of plugin method to call
-     * @param mixed  $parameter  Value that gets passed instead of the model instance.
-     */
-    protected function _callPluginMethod($methodname, $parameter = null)
-    {
-        try {
-            if (null === $parameter) {
-                $param = $this;
-            }
-            else {
-                $param = $parameter;
-            }
-            foreach ($this->_plugins as $name=>$plugin) {
-                $plugin->$methodname($param);
-            }
-        } catch (Exception $ex) {
-            throw new Opus_Model_Exception(
-                'Plugin ' . $name . ' failed in ' . $methodname
-                . ' with ' . $ex->getMessage()
-            );
         }
     }
 
