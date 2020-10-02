920c409f22d12410c6ecea11f96545de4dba38ad
j3nsch <j3nsch@gmail.com>
2020-10-02 21:01:12 +0200

    OPUSVIER-4369 Add properties for documents and persons.

---
+++ library/Opus/Model/PropertySupportInterface.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Framework
+ * @package     Opus_Model
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2020, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ */
+
+/**
+ * Interface for model classes supporting internal properties.
+ *
+ * TODO pass through some exceptions from Opus_Model_Properties?
+ */
+interface Opus_Model_PropertySupportInterface
+{
+
+    /**
+     * Returns identifier for model.
+     * @return int|null
+     *
+     * TODO rename to getModelId ?
+     */
+    public function getId();
+
+    /**
+     * Sets property for model.
+     * @param string $key Name of property
+     * @param string $value Value of property
+     */
+    public function setProperty($key, $value);
+
+    /**
+     * Returns value of property.
+     * @param string $key Name of property
+     * @return string|null
+     */
+    public function getProperty($key);
+
+    /**
+     * Returns identifier for model type.
+     * @return string
+     */
+    public function getModelType();
+}
