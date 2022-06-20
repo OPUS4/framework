40a8c6b3513f7097eeeeeac381d7b83d1d2d0647
j3nsch <j3nsch@gmail.com>
2022-06-20 18:33:21 +0200

    #253 Fix coding style

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -81,6 +81,6 @@ $options['opus']['disableDatabaseVersionCheck'] = true;
 $application->setOptions($options);
 
 $resourcesOption = $options['opus']['bootstrap']['resources'];
-$resources = preg_split('/(\s*,\s*)+/', $resourcesOption);
+$resources       = preg_split('/(\s*,\s*)+/', $resourcesOption);
 
 $application->bootstrap($resources);
