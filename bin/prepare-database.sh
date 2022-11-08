5fa709f86cab560f7932a7f68e09f612b32a85b4
j3nsch <j3nsch@gmail.com>
2022-11-08 16:12:39 +0100

    #319 Added options; Install script to vendor/bin

--- bin/prepare-database.sh
+++ bin/prepare-database.sh
@@ -36,17 +36,7 @@
 # IMPORTANT: This script is also used by other OPUS 4 packages that require
 #            a database for testing.
 #
-# TODO dropping users prevents this script from being used to setup multiple databases with the same users (testing)
-#      For testing on GitHub dropping users doesn't matter because we start with a fresh database.
-# TODO create new database without creating new users
-#
-# TODO interactive mode
-# TODO MySQL root password entered interactively by default, so it does not appear in logs
-# TODO replace underlines in option names (dashes seem more common and are easier)
-#
-# TODO variable for MySQL client
-# TODO variables for host and port
-# TODO variable for MySQL "root" username
+# TODO make interactive mode default (requires updating usage across all packages)
 #
 # TODO merge with setup.sh (get rid of setup.sh or remove redundancy)
 # TODO creating database.ini file with config ? (replacing/removing config.ini.template)
@@ -57,12 +47,16 @@
 #
 
 # Define variables and their default values
-# root_pwd='root'
-admin_name='opus4admin'
-admin_pwd='opusadminpwd'
-user_name='opus4'
-user_pwd='opususerpwd'
-database_name='opusdb'
+sqluser='root'
+sqlpwd='root'
+admin='opus4admin'
+adminpwd='opusadminpwd'
+user='opus4'
+userpwd='opususerpwd'
+dbname='opusdb'
+host='localhost'
+port='3306'
+mysql='mysql'
 
 # Print command line help to stderr
 display_help() {
@@ -70,18 +64,26 @@ display_help() {
     echo
     echo "Options (default values given in parentheses):"
     echo
-    echo "  --root_pwd        Root password ($root_pwd)"
-    echo "  --admin_name      Admin name ($admin_name)"
-    echo "  --admin_pwd       Admin password ($admin_pwd)"
-    echo "  --user_name       User name ($user_name)"
-    echo "  --user_pwd        User password ($user_pwd)"
-    echo "  --database_name   Database name ($database_name)"
+    echo "  --help        (-h)    Print out help"
+    echo "  --interactive (-i)    Ask for SQL root password interactively"
+    echo
+    echo "  --sqluser             SQL root user      ($sqluser)"
+    echo "  --sqlpwd              SQL root password  ($sqlpwd)"
+    echo "  --admin               Admin name         ($admin)"
+    echo "  --adminpwd            Admin password     ($adminpwd)"
+    echo "  --user                User name          ($user)"
+    echo "  --userpwd             User password      ($userpwd)"
+    echo "  --dbname              Database name      ($dbname)"
+    echo "  --host                MySQL host         ($host)"
+    echo "  --port                MySQL port         ($port)"
+    echo "  --mysql               MySQL client       ($mysql)"
     echo
     echo "Examples:"
     echo
     echo "  $0"
     echo "  $0 --help"
-    echo "  $0 --admin_pwd ADMINPWD --user_pwd USERPWD"
+    echo "  $0 --adminpwd ADMINPWD --userpwd USERPWD"
+    echo "  $0 -i --dbname opusdbtest"
     echo
     exit 1
 }
@@ -121,27 +123,23 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-if [[ -z $root_pwd ]]; then
+if [[ -z $sqlpwd ]]; then
     if [ $interactive_enabled != 1 ]; then
-        root_pwd='root'
+        sqlpwd='root'
       else
         # Querying MySQL root password
-        [[ -z $root_pwd ]] && read -p "MySQL root user password: " -s root_pwd
+        [[ -z $sqlpwd ]] && read -p "MySQL root user password: " -s sqlpwd
     fi
 fi
 
-# TODO remove following SQL permanently
-#     DROP USER IF EXISTS '$admin_name'@'localhost';
-#     DROP USER IF EXISTS '$user_name'@'localhost';
-
 sql=$(cat <<-ENDSTRING
-    CREATE DATABASE IF NOT EXISTS $database_name DEFAULT CHARACTER SET = UTF8 DEFAULT COLLATE = UTF8_GENERAL_CI;
-    CREATE USER IF NOT EXISTS '$admin_name'@'localhost' IDENTIFIED WITH mysql_native_password BY '${admin_pwd}';
-    GRANT ALL PRIVILEGES ON $database_name.* TO '$admin_name'@'localhost';
-    CREATE USER IF NOT EXISTS '$user_name'@'localhost' IDENTIFIED WITH mysql_native_password BY '$user_pwd';
-    GRANT SELECT,INSERT,UPDATE,DELETE ON $database_name.* TO '$user_name'@'localhost';
+    CREATE DATABASE IF NOT EXISTS $dbname DEFAULT CHARACTER SET = UTF8 DEFAULT COLLATE = UTF8_GENERAL_CI;
+    CREATE USER IF NOT EXISTS '$admin'@'localhost' IDENTIFIED WITH mysql_native_password BY '${adminpwd}';
+    GRANT ALL PRIVILEGES ON $dbname.* TO '$admin'@'localhost';
+    CREATE USER IF NOT EXISTS '$user'@'localhost' IDENTIFIED WITH mysql_native_password BY '$userpwd';
+    GRANT SELECT,INSERT,UPDATE,DELETE ON $dbname.* TO '$user'@'localhost';
     FLUSH PRIVILEGES;
 ENDSTRING
 )
 
-export MYSQL_PWD=$root_pwd && mysql --default-character-set=utf8 -h 'localhost' -P '3306' -u 'root' -v -e "$sql"
+export MYSQL_PWD=$sqlpwd && mysql --default-character-set=utf8 -h $host -P $port -u $sqluser -v -e "$sql"
