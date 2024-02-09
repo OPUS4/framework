7c68d176c9a7cab419fac0dac3bacc97d072ce6a
j3nsch <j3nsch@gmail.com>
2024-02-09 15:35:03 +0100

    #342 Convert old publication_state entries

--- db/schema/022-Modify-field-publication-state.sql
+++ db/schema/022-Modify-field-publication-state.sql
@@ -3,7 +3,19 @@ START TRANSACTION;
 -- Replace identifier type ENUM with VARCHAR to allow any type
 
 ALTER TABLE `documents`
-    MODIFY COLUMN `publication_state` ENUM('draft', 'acceptedVersion', 'submittedVersion', 'publishedVersion', 'updatedVersion') COMMENT 'Version of publication.';
+    MODIFY COLUMN `publication_state` ENUM(
+        'draft', 'accepted', 'submitted', 'published', 'updated',
+        'acceptedVersion', 'submittedVersion', 'publishedVersion', 'updatedVersion') COMMENT 'Version of publication.';
+
+UPDATE `documents` SET `publication_state` = 'acceptedVersion' WHERE `publication_state` = 'accepted';
+UPDATE `documents` SET `publication_state` = 'submittedVersion' WHERE `publication_state` = 'submitted';
+UPDATE `documents` SET `publication_state` = 'publishedVersion' WHERE `publication_state` = 'published';
+UPDATE `documents` SET `publication_state` = 'updatedVersion' WHERE `publication_state` = 'updated';
+
+ALTER TABLE `documents`
+    MODIFY COLUMN `publication_state` ENUM(
+        'draft', 'acceptedVersion', 'submittedVersion', 'publishedVersion', 'updatedVersion'
+    ) COMMENT 'Version of publication.';
 
 -- Update database version
 
